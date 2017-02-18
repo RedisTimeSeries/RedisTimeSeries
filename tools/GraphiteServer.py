0e035abd80ca7c41357d1f4b8cf88a8fb5f17eea
Danni Moiseyev <danni@redislabs.com>
2017-02-18 14:48:57 +0200

    Added tools/ directory with GraphiteServer.py

---
+++ tools/GraphiteServer.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import re
+import argparse
+import redis
+from gevent.server import StreamServer
+
+
+REDIS_POOL = None
+GRAPHITE_PROTO_RE = re.compile(r"(.*?)\s+([\d.]+)\s+(\d+)")
+
+def process_connection(socket, _):
+    """
+    Per-Connection handler, read all lines and send to redis
+    """
+    # using a makefile because we want to use readline()
+    rfileobj = socket.makefile(mode='rb')
+    redis_client = redis.Redis(connection_pool=REDIS_POOL)
+    while True:
+        line = rfileobj.readline()
+        if not line:
+            # client disconnect
+            break
+        data = GRAPHITE_PROTO_RE.findall(line)
+        if data:
+            # the line is in graphite format
+            try:
+                path, value, timestamp = data[0]
+                value = float(value)
+                timestamp = int(timestamp)
+            except Exception as ex:
+                print("could parse an element %s" % ex)
+                break
+
+            try:
+                redis_client.execute_command("ts.add", path, timestamp, value)
+            except redis.ResponseError as ex:
+                # small hack, for performance reasons its better to first try to add an metric
+                # instead of checking per metric if it exists or not
+                if 'the key does not exists' in ex.message:
+                    redis_client.execute_command("ts.create", path, 60, 10)
+                    redis_client.execute_command("ts.add", path, timestamp, value)
+                else:
+                    raise
+
+        else:
+            print("line is not in graphite format: %s" % line)
+            break
+    rfileobj.close()
+
+
+def main():
+    global REDIS_POOL
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", help="server address to listen to", default="127.0.0.1")
+    parser.add_argument("--port", help="port number to listen to", default=2003, type=int)
+    parser.add_argument("--redis-server", help="redis server address")
+    parser.add_argument("--redis-port", help="redis server port", type=int)
+    args = parser.parse_args()
+
+    REDIS_POOL = redis.ConnectionPool(host='localhost', port=6379)
+
+    server = StreamServer((args.host, args.port), process_connection)
+    print('Starting Graphite server on %s:%s' % (args.host, args.port))
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
---
+++ tools/README.md
@@ -0,0 +1,62 @@
+# Tools
+
+## Overview
+This folder contains some tools to be used with other metrics collection systems
+
+## GraphiteServer.py
+### Overview
+This is a simple python based, greenlet TCP server for accepting incoming metrics in graphite protocol
+
+### Protocol
+```
+<metric path> <metric value> <metric timestamp>
+```
+Where
+* metric path - String
+* metric value - Float
+* metric timestamp - int
+
+### Usage
+```
+usage: GraphiteServer.py [-h] [--host HOST] [--port PORT]
+                         [--redis-server REDIS_SERVER]
+                         [--redis-port REDIS_PORT]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --host HOST           server address to listen to
+  --port PORT           port number to listen to
+  --redis-server REDIS_SERVER
+                        redis server address
+  --redis-port REDIS_PORT
+                        redis server port
+```
+
+### Dependencies
+To install the needed dependencies just run: `pip install -r requirements.txt`
+
+### Usage with Statsd
+Since this tools uses the same protocol as graphite, just configure your StatsD server to send it to the appropriate server.
+
+#### Example
+GraphiteServer.py cmd:
+```
+./GraphiteServer.py --host localhost --port 16000 --redis-server localhost --redis-port 6397
+```
+
+StatsD configuration:
+```
+{
+  graphitePort: 16000
+, graphiteHost: "localhost"
+, port: 8125
+, backends: [ "./backends/graphite" ]
+}
+```
+
+#### Note regarding metric naming
+The metrics names will be exactly as StatsD sends them for example: `stats.gauges.load`.
+There's no way at the moment to query the keys as part of a tree, so to get the metrics data you should use the fullpath:
+```
+ts.range stats.gauges.load 1487262000 1487262890
+```
