81d167c564d64d84a5a09c915e1725cbf48a4005
Guy Korland <gkorland@gmail.com>
2019-06-10 18:35:01 +0300

    fix #77 delete rules references when a key is deleted

--- src/tests/test_module.py
+++ src/tests/test_module.py
@@ -147,6 +147,7 @@ class RedisTimeseriesTests(ModuleTestCase(os.path.dirname(os.path.abspath(__file
               'lastTimestamp': start_ts + samples_count - 1,
               'maxSamplesPerChunk': 360L,
               'retentionSecs': 0L,
+              'sourceKey': '',
               'rules': []}
             actual_result = self._get_ts_info(r, 'tester')
             assert expected_result == actual_result
@@ -176,6 +177,7 @@ class RedisTimeseriesTests(ModuleTestCase(os.path.dirname(os.path.abspath(__file
                                'lastTimestamp': 1511887408L,
                                'maxSamplesPerChunk': 360L,
                                'retentionSecs': 0L,
+                               'sourceKey': '',
                                'rules': [['tester_agg_avg_10', 10L, 'AVG'], ['tester_agg_max_10', 10L, 'MAX']]}
             actual_result = self._get_ts_info(r, 'tester')
             assert expected_result == actual_result
@@ -337,7 +339,26 @@ class RedisTimeseriesTests(ModuleTestCase(os.path.dirname(os.path.abspath(__file
                                  'maxSamplesPerChunk': 360L,
                                  'retentionSecs': 0L,
                                  'labels': [],
+                                 'sourceKey': '',
                                  'rules': [['tester_agg_max_10', 10L, 'AVG']]}
+            
+            
+    def test_delete_key(self):
+        with self.redis() as r:
+            assert r.execute_command('TS.CREATE', 'tester', 'CHUNK_SIZE', '360')
+            assert r.execute_command('TS.CREATE', 'tester_agg_max_10')
+            assert r.execute_command('TS.CREATERULE', 'tester', 'tester_agg_max_10', 'AGGREGATION', 'avg', 10)
+            assert r.delete('tester_agg_max_10')
+            assert len(self._get_ts_info(r, 'tester')['rules']) == 0
+            
+            assert r.execute_command('TS.CREATE', 'tester_agg_max_10')
+            assert r.execute_command('TS.CREATERULE', 'tester', 'tester_agg_max_10', 'AGGREGATION', 'avg', 11)
+            assert r.delete('tester')
+            assert self._get_ts_info(r, 'tester_agg_max_10')['sourceKey'] == ''
+            
+            assert r.execute_command('TS.CREATE', 'tester')
+            assert r.execute_command('TS.CREATERULE', 'tester', 'tester_agg_max_10', 'AGGREGATION', 'avg', 12)
+            assert len(self._get_ts_info(r, 'tester')['rules']) == 1
     
     def test_create_compaction_rule_without_dest_series(self):
         with self.redis() as r:
@@ -571,6 +592,8 @@ class RedisTimeseriesTests(ModuleTestCase(os.path.dirname(os.path.abspath(__file
                     ['name',
                     'blabla']
                 ],
+                'sourceKey',
+                '',
                 'rules',
                 []
             ]
@@ -596,6 +619,8 @@ class RedisTimeseriesTests(ModuleTestCase(os.path.dirname(os.path.abspath(__file
                         'earth'
                     ]
                 ],
+                'sourceKey',
+                '',
                 'rules',
                 []
             ]
