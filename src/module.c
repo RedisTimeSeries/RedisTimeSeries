6aa928cfb3c6c29efcfb4d4f9721dcb624d7d142
Ariel <ashtul@gmail.com>
2019-09-04 15:02:09 +0300

    refactoring system timestamp

--- src/module.c
+++ src/module.c
@@ -861,17 +861,10 @@ int TSDB_incrby(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 
     long long currentUpdatedTime = -1;
     int timestampLoc = RMUtil_ArgIndex("TIMESTAMP", argv, argc);
-    if (timestampLoc > 0) {
-        if ((RedisModule_StringToLongLong(argv[timestampLoc + 1], (long long *) &currentUpdatedTime) != REDISMODULE_OK)) {
-            // if timestamp is "*", take current time (automatic timestamp)
-            if(RMUtil_StringEqualsC(argv[timestampLoc + 1], "*")) {
-                currentUpdatedTime = RedisModule_Milliseconds();
-            } else {
-                return RedisModule_ReplyWithError(ctx, "TSDB: invalid timestamp");
-            }
-        }
-    } else {
+    if (timestampLoc == -1 || RMUtil_StringEqualsC(argv[timestampLoc + 1], "*")) {
         currentUpdatedTime = RedisModule_Milliseconds();
+    } else if(RedisModule_StringToLongLong(argv[timestampLoc + 1], (long long *) &currentUpdatedTime) != REDISMODULE_OK) {
+        return RedisModule_ReplyWithError(ctx, "TSDB: invalid timestamp");
     }
 
     double result;
