78f787ea7e41415480e31065764fc8c69a091592
Ariel <ashtul@gmail.com>
2020-01-12 16:04:42 +0200

    Fix Issue #299

--- src/module.c
+++ src/module.c
@@ -509,7 +509,7 @@ int ReplySeriesRange(RedisModuleCtx *ctx, Series *series, api_timestamp_t start_
         AggregationClass *aggObject, int64_t time_delta, long long maxResults) {
     Sample sample;
     long long arraylen = 0;
-    timestamp_t last_agg_timestamp = 0;
+    timestamp_t last_agg_timestamp;
 
     // In case a retention is set shouldn't return chunks older than the retention 
     if(series->retentionTime){
@@ -519,9 +519,12 @@ int ReplySeriesRange(RedisModuleCtx *ctx, Series *series, api_timestamp_t start_
     SeriesIterator iterator = SeriesQuery(series, start_ts, end_ts);
 
     void *context = NULL;
-    if (aggObject != NULL)
+    if (aggObject != NULL) {
         context = aggObject->createContext();
-    
+        timestamp_t initTS = series->funcs->GetFirstTimestamp(iterator.currentChunk);
+        last_agg_timestamp = initTS - (initTS % time_delta);
+    }
+
     RedisModule_ReplyWithArray(ctx, REDISMODULE_POSTPONED_ARRAY_LEN);
     while (SeriesIteratorGetNext(&iterator, &sample) != 0 &&
                     (maxResults == -1 || arraylen < maxResults)) {
@@ -534,10 +537,8 @@ int ReplySeriesRange(RedisModuleCtx *ctx, Series *series, api_timestamp_t start_
         } else {
             timestamp_t current_timestamp = sample.timestamp - (sample.timestamp % time_delta);
             if (current_timestamp > last_agg_timestamp) {
-                if (last_agg_timestamp != 0) {
-                    ReplyWithAggValue(ctx, last_agg_timestamp, aggObject, context);
-                    arraylen++;
-                }
+                ReplyWithAggValue(ctx, last_agg_timestamp, aggObject, context);
+                arraylen++;
                 last_agg_timestamp = current_timestamp;
             }
             aggObject->appendValue(context, sample.value);
