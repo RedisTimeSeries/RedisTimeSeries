46baff18f679e21f39ddae968373d25c9b6a43c4
Ariel <ashtul@gmail.com>
2020-03-03 15:13:41 +0200

    using 128bit var

--- src/gorilla.c
+++ src/gorilla.c
@@ -115,26 +115,19 @@
 
 // 2^bit
 static inline u_int64_t BIT(u_int64_t bit) {
-    if (__glibc_unlikely(bit > 63)) {
-        return 0ULL;
-    }
-    return (1ULL << bit);
+    __uint128_t tmp = 1;
+    return (__uint64_t)(tmp << bit);
 }
 
 // the LSB `bits` turned on
 static inline u_int64_t MASK(u_int64_t bits) {
-    if (__glibc_unlikely(bits > 63)) {
-        return -1ULL;
-    }
-    return (1ULL << bits) - 1;
+    __uint128_t tmp = 1;
+    return (__uint64_t)((tmp << bits) - 1);
 }
 
 // Clear most significant bits from position `bits`
 static inline u_int64_t LSB(u_int64_t x, u_int64_t bits) {
-    if (__glibc_unlikely(bits > 63)) {
-        return x;
-    }
-    return x & ((1ULL << bits) - 1);
+    return x & MASK(bits);
 }
 
 /* 
