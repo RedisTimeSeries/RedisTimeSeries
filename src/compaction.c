61fb8252e42420060ce9840183fd9049ac89d3f2
Danni Moiseyev <danni@redislabs.com>
2020-06-08 18:11:14 +0300

    Add clang-format (#411)
    * Introduce clang-format
    * reformat files according to the clang-format style guide

--- src/compaction.c
+++ src/compaction.c
@@ -1,33 +1,38 @@
 /*
-* Copyright 2018-2019 Redis Labs Ltd. and Contributors
-*
-* This file is available under the Redis Labs Source Available License Agreement
-*/
-#include <ctype.h>
-#include <string.h>
-#include <math.h>           // sqrt
+ * Copyright 2018-2019 Redis Labs Ltd. and Contributors
+ *
+ * This file is available under the Redis Labs Source Available License Agreement
+ */
 #include "compaction.h"
-#include "rmutil/alloc.h"
 
-typedef struct MaxMinContext {
+#include <ctype.h>
+#include <math.h> // sqrt
+#include <string.h>
+#include <rmutil/alloc.h>
+
+typedef struct MaxMinContext
+{
     double minValue;
     double maxValue;
     char isResetted;
 } MaxMinContext;
 
-typedef struct SingleValueContext {
+typedef struct SingleValueContext
+{
     double value;
     char isResetted;
 } SingleValueContext;
 
-typedef struct AvgContext {
+typedef struct AvgContext
+{
     double val;
     double cnt;
 } AvgContext;
 
-typedef struct StdContext {
+typedef struct StdContext
+{
     double sum;
-    double sum_2;   // sum of (values^2)
+    double sum_2; // sum of (values^2)
     u_int64_t cnt;
 } StdContext;
 
@@ -54,7 +59,7 @@ void SingleValueWriteContext(void *contextPtr, RedisModuleIO *io) {
     RedisModule_SaveDouble(io, context->value);
 }
 
-void SingleValueReadContext(void *contextPtr, RedisModuleIO *io){
+void SingleValueReadContext(void *contextPtr, RedisModuleIO *io) {
     SingleValueContext *context = (SingleValueContext *)contextPtr;
     context->value = RedisModule_LoadDouble(io);
 }
@@ -62,11 +67,11 @@ void SingleValueReadContext(void *contextPtr, RedisModuleIO *io){
 void *AvgCreateContext() {
     AvgContext *context = (AvgContext *)malloc(sizeof(AvgContext));
     context->cnt = 0;
-    context->val =0;
+    context->val = 0;
     return context;
 }
 
-void AvgAddValue(void *contextPtr, double value){
+void AvgAddValue(void *contextPtr, double value) {
     AvgContext *context = (AvgContext *)contextPtr;
     context->val += value;
     context->cnt++;
@@ -74,7 +79,8 @@ void AvgAddValue(void *contextPtr, double value){
 
 double AvgFinalize(void *contextPtr) {
     AvgContext *context = (AvgContext *)contextPtr;
-    if (context->cnt == 0) return 0;
+    if (context->cnt == 0)
+        return 0;
     return context->val / context->cnt;
 }
 
@@ -90,7 +96,7 @@ void AvgWriteContext(void *contextPtr, RedisModuleIO *io) {
     RedisModule_SaveDouble(io, context->cnt);
 }
 
-void AvgReadContext(void *contextPtr, RedisModuleIO *io){
+void AvgReadContext(void *contextPtr, RedisModuleIO *io) {
     AvgContext *context = (AvgContext *)contextPtr;
     context->val = RedisModule_LoadDouble(io);
     context->cnt = RedisModule_LoadDouble(io);
@@ -104,15 +110,17 @@ void *StdCreateContext() {
     return context;
 }
 
-void StdAddValue(void *contextPtr, double value){
+void StdAddValue(void *contextPtr, double value) {
     StdContext *context = (StdContext *)contextPtr;
     ++context->cnt;
     context->sum += value;
-    context->sum_2 += value * value; 
+    context->sum_2 += value * value;
 }
 
 static inline double variance(double sum, double sum_2, double count) {
-    if (count == 0) { return 0; }
+    if (count == 0) {
+        return 0;
+    }
 
     /*  var(X) = sum((x_i - E[X])^2)
      *  = sum(x_i^2) - 2 * sum(x_i) * E[X] + E^2[X] */
@@ -128,7 +136,9 @@ double VarPopulationFinalize(void *contextPtr) {
 double VarSamplesFinalize(void *contextPtr) {
     StdContext *context = (StdContext *)contextPtr;
     uint64_t count = context->cnt;
-    if(count == 1) { return 0; }
+    if (count == 1) {
+        return 0;
+    }
     return variance(context->sum, context->sum_2, count) * count / (count - 1);
 }
 
@@ -154,7 +164,7 @@ void StdWriteContext(void *contextPtr, RedisModuleIO *io) {
     RedisModule_SaveUnsigned(io, context->cnt);
 }
 
-void StdReadContext(void *contextPtr, RedisModuleIO *io){
+void StdReadContext(void *contextPtr, RedisModuleIO *io) {
     StdContext *context = (StdContext *)contextPtr;
     context->sum = RedisModule_LoadDouble(io);
     context->sum_2 = RedisModule_LoadDouble(io);
@@ -165,55 +175,45 @@ void rm_free(void *ptr) {
     free(ptr);
 }
 
-static AggregationClass aggAvg = {
-    .createContext = AvgCreateContext,
-    .appendValue = AvgAddValue,
-    .freeContext = rm_free,
-    .finalize = AvgFinalize,
-    .writeContext = AvgWriteContext,
-    .readContext = AvgReadContext,
-    .resetContext = AvgReset
-};
+static AggregationClass aggAvg = { .createContext = AvgCreateContext,
+                                   .appendValue = AvgAddValue,
+                                   .freeContext = rm_free,
+                                   .finalize = AvgFinalize,
+                                   .writeContext = AvgWriteContext,
+                                   .readContext = AvgReadContext,
+                                   .resetContext = AvgReset };
 
-static AggregationClass aggStdP = {
-    .createContext = StdCreateContext,
-    .appendValue = StdAddValue,
-    .freeContext = rm_free,
-    .finalize = StdPopulationFinalize,
-    .writeContext = StdWriteContext,
-    .readContext = StdReadContext,
-    .resetContext = StdReset
-};
+static AggregationClass aggStdP = { .createContext = StdCreateContext,
+                                    .appendValue = StdAddValue,
+                                    .freeContext = rm_free,
+                                    .finalize = StdPopulationFinalize,
+                                    .writeContext = StdWriteContext,
+                                    .readContext = StdReadContext,
+                                    .resetContext = StdReset };
 
-static AggregationClass aggStdS = {
-    .createContext = StdCreateContext,
-    .appendValue = StdAddValue,
-    .freeContext = rm_free,
-    .finalize = StdSamplesFinalize,
-    .writeContext = StdWriteContext,
-    .readContext = StdReadContext,
-    .resetContext = StdReset
-};
+static AggregationClass aggStdS = { .createContext = StdCreateContext,
+                                    .appendValue = StdAddValue,
+                                    .freeContext = rm_free,
+                                    .finalize = StdSamplesFinalize,
+                                    .writeContext = StdWriteContext,
+                                    .readContext = StdReadContext,
+                                    .resetContext = StdReset };
 
-static AggregationClass aggVarP = {
-    .createContext = StdCreateContext,
-    .appendValue = StdAddValue,
-    .freeContext = rm_free,
-    .finalize = VarPopulationFinalize,
-    .writeContext = StdWriteContext,
-    .readContext = StdReadContext,
-    .resetContext = StdReset
-};
+static AggregationClass aggVarP = { .createContext = StdCreateContext,
+                                    .appendValue = StdAddValue,
+                                    .freeContext = rm_free,
+                                    .finalize = VarPopulationFinalize,
+                                    .writeContext = StdWriteContext,
+                                    .readContext = StdReadContext,
+                                    .resetContext = StdReset };
 
-static AggregationClass aggVarS = {
-    .createContext = StdCreateContext,
-    .appendValue = StdAddValue,
-    .freeContext = rm_free,
-    .finalize = VarSamplesFinalize,
-    .writeContext = StdWriteContext,
-    .readContext = StdReadContext,
-    .resetContext = StdReset
-};
+static AggregationClass aggVarS = { .createContext = StdCreateContext,
+                                    .appendValue = StdAddValue,
+                                    .freeContext = rm_free,
+                                    .finalize = VarSamplesFinalize,
+                                    .writeContext = StdWriteContext,
+                                    .readContext = StdReadContext,
+                                    .resetContext = StdReset };
 
 void *MaxMinCreateContext() {
     MaxMinContext *context = (MaxMinContext *)malloc(sizeof(MaxMinContext));
@@ -299,75 +299,61 @@ void LastAppendValue(void *contextPtr, double value) {
     context->value = value;
 }
 
-static AggregationClass aggMax = {
-    .createContext = MaxMinCreateContext,
-    .appendValue = MaxMinAppendValue,
-    .freeContext = rm_free,
-    .finalize = MaxFinalize,
-    .writeContext = MaxMinWriteContext,
-    .readContext = MaxMinReadContext,
-    .resetContext = MaxMinReset
-};
+static AggregationClass aggMax = { .createContext = MaxMinCreateContext,
+                                   .appendValue = MaxMinAppendValue,
+                                   .freeContext = rm_free,
+                                   .finalize = MaxFinalize,
+                                   .writeContext = MaxMinWriteContext,
+                                   .readContext = MaxMinReadContext,
+                                   .resetContext = MaxMinReset };
 
-static AggregationClass aggMin = {
-    .createContext = MaxMinCreateContext,
-    .appendValue = MaxMinAppendValue,
-    .freeContext = rm_free,
-    .finalize = MinFinalize,
-    .writeContext = MaxMinWriteContext,
-    .readContext = MaxMinReadContext,
-    .resetContext = MaxMinReset
-};
+static AggregationClass aggMin = { .createContext = MaxMinCreateContext,
+                                   .appendValue = MaxMinAppendValue,
+                                   .freeContext = rm_free,
+                                   .finalize = MinFinalize,
+                                   .writeContext = MaxMinWriteContext,
+                                   .readContext = MaxMinReadContext,
+                                   .resetContext = MaxMinReset };
 
-static AggregationClass aggSum = {
-    .createContext = SingleValueCreateContext,
-    .appendValue = SumAppendValue,
-    .freeContext = rm_free,
-    .finalize = SingleValueFinalize,
-    .writeContext = SingleValueWriteContext,
-    .readContext = SingleValueReadContext,
-    .resetContext = SingleValueReset
-};
+static AggregationClass aggSum = { .createContext = SingleValueCreateContext,
+                                   .appendValue = SumAppendValue,
+                                   .freeContext = rm_free,
+                                   .finalize = SingleValueFinalize,
+                                   .writeContext = SingleValueWriteContext,
+                                   .readContext = SingleValueReadContext,
+                                   .resetContext = SingleValueReset };
 
-static AggregationClass aggCount = {
-    .createContext = SingleValueCreateContext,
-    .appendValue = CountAppendValue,
-    .freeContext = rm_free,
-    .finalize = SingleValueFinalize,
-    .writeContext = SingleValueWriteContext,
-    .readContext = SingleValueReadContext,
-    .resetContext = SingleValueReset
-};
+static AggregationClass aggCount = { .createContext = SingleValueCreateContext,
+                                     .appendValue = CountAppendValue,
+                                     .freeContext = rm_free,
+                                     .finalize = SingleValueFinalize,
+                                     .writeContext = SingleValueWriteContext,
+                                     .readContext = SingleValueReadContext,
+                                     .resetContext = SingleValueReset };
 
-static AggregationClass aggFirst = {
-    .createContext = SingleValueCreateContext,
-    .appendValue = FirstAppendValue,
-    .freeContext = rm_free,
-    .finalize = SingleValueFinalize,
-    .writeContext = SingleValueWriteContext,
-    .readContext = SingleValueReadContext,
-    .resetContext = SingleValueReset
-};
+static AggregationClass aggFirst = { .createContext = SingleValueCreateContext,
+                                     .appendValue = FirstAppendValue,
+                                     .freeContext = rm_free,
+                                     .finalize = SingleValueFinalize,
+                                     .writeContext = SingleValueWriteContext,
+                                     .readContext = SingleValueReadContext,
+                                     .resetContext = SingleValueReset };
 
-static AggregationClass aggLast = {
-    .createContext = SingleValueCreateContext,
-    .appendValue = LastAppendValue,
-    .freeContext = rm_free,
-    .finalize = SingleValueFinalize,
-    .writeContext = SingleValueWriteContext,
-    .readContext = SingleValueReadContext,
-    .resetContext = SingleValueReset
-};
+static AggregationClass aggLast = { .createContext = SingleValueCreateContext,
+                                    .appendValue = LastAppendValue,
+                                    .freeContext = rm_free,
+                                    .finalize = SingleValueFinalize,
+                                    .writeContext = SingleValueWriteContext,
+                                    .readContext = SingleValueReadContext,
+                                    .resetContext = SingleValueReset };
 
-static AggregationClass aggRange = {
-    .createContext = MaxMinCreateContext,
-    .appendValue = MaxMinAppendValue,
-    .freeContext = rm_free,
-    .finalize = RangeFinalize,
-    .writeContext = MaxMinWriteContext,
-    .readContext = MaxMinReadContext,
-    .resetContext = MaxMinReset
-};
+static AggregationClass aggRange = { .createContext = MaxMinCreateContext,
+                                     .appendValue = MaxMinAppendValue,
+                                     .freeContext = rm_free,
+                                     .finalize = RangeFinalize,
+                                     .writeContext = MaxMinWriteContext,
+                                     .readContext = MaxMinReadContext,
+                                     .resetContext = MaxMinReset };
 
 int StringAggTypeToEnum(const char *agg_type) {
     return StringLenAggTypeToEnum(agg_type, strlen(agg_type));
@@ -380,43 +366,43 @@ int RMStringLenAggTypeToEnum(RedisModuleString *aggTypeStr) {
 }
 
 int StringLenAggTypeToEnum(const char *agg_type, size_t len) {
-	int result = TS_AGG_INVALID;
-	char agg_type_lower[len];
-	for(int i = 0; i < len; i++){
-		agg_type_lower[i] = tolower(agg_type[i]);
-	}
-	if(len == 3){
-		if (strncmp(agg_type_lower, "min", len) == 0 && len == 3){
-			result = TS_AGG_MIN;
-		} else if (strncmp(agg_type_lower, "max", len) == 0) {
-			result = TS_AGG_MAX;
-		} else if (strncmp(agg_type_lower, "sum", len) == 0) {
-			result = TS_AGG_SUM;
-		} else if (strncmp(agg_type_lower, "avg", len) == 0) {
-			result = TS_AGG_AVG;
-		}
-	} else if (len == 4){
-		if (strncmp(agg_type_lower, "last", len) == 0) {
-			result = TS_AGG_LAST;
-		}
-	} else if (len == 5){
-		if (strncmp(agg_type_lower, "count", len) == 0) {
-			result = TS_AGG_COUNT;
-		} else if (strncmp(agg_type_lower, "range", len) == 0) {
-			result = TS_AGG_RANGE;
-		} else if (strncmp(agg_type_lower, "first", len) == 0) {
-			result = TS_AGG_FIRST;
-		} else if (strncmp(agg_type_lower, "std.p", len) == 0) {
-			result = TS_AGG_STD_P;
-		} else if (strncmp(agg_type_lower, "std.s", len) == 0) {
-			result = TS_AGG_STD_S;
-		} else if (strncmp(agg_type_lower, "var.p", len) == 0) {
-			result = TS_AGG_VAR_P;
-		} else if (strncmp(agg_type_lower, "var.s", len) == 0) {
-			result = TS_AGG_VAR_S;
-		}
-	}
-	return result;
+    int result = TS_AGG_INVALID;
+    char agg_type_lower[len];
+    for (int i = 0; i < len; i++) {
+        agg_type_lower[i] = tolower(agg_type[i]);
+    }
+    if (len == 3) {
+        if (strncmp(agg_type_lower, "min", len) == 0 && len == 3) {
+            result = TS_AGG_MIN;
+        } else if (strncmp(agg_type_lower, "max", len) == 0) {
+            result = TS_AGG_MAX;
+        } else if (strncmp(agg_type_lower, "sum", len) == 0) {
+            result = TS_AGG_SUM;
+        } else if (strncmp(agg_type_lower, "avg", len) == 0) {
+            result = TS_AGG_AVG;
+        }
+    } else if (len == 4) {
+        if (strncmp(agg_type_lower, "last", len) == 0) {
+            result = TS_AGG_LAST;
+        }
+    } else if (len == 5) {
+        if (strncmp(agg_type_lower, "count", len) == 0) {
+            result = TS_AGG_COUNT;
+        } else if (strncmp(agg_type_lower, "range", len) == 0) {
+            result = TS_AGG_RANGE;
+        } else if (strncmp(agg_type_lower, "first", len) == 0) {
+            result = TS_AGG_FIRST;
+        } else if (strncmp(agg_type_lower, "std.p", len) == 0) {
+            result = TS_AGG_STD_P;
+        } else if (strncmp(agg_type_lower, "std.s", len) == 0) {
+            result = TS_AGG_STD_S;
+        } else if (strncmp(agg_type_lower, "var.p", len) == 0) {
+            result = TS_AGG_VAR_P;
+        } else if (strncmp(agg_type_lower, "var.s", len) == 0) {
+            result = TS_AGG_VAR_S;
+        }
+    }
+    return result;
 }
 
 const char *AggTypeEnumToString(TS_AGG_TYPES_T aggType) {
--- src/config.c
+++ src/config.c
@@ -1,16 +1,18 @@
 /*
-* Copyright 2018-2019 Redis Labs Ltd. and Contributors
-*
-* This file is available under the Redis Labs Source Available License Agreement
-*/
-#include "redismodule.h"
-#include "rmutil/util.h"
-#include "rmutil/strings.h"
+ * Copyright 2018-2019 Redis Labs Ltd. and Contributors
+ *
+ * This file is available under the Redis Labs Source Available License Agreement
+ */
 #include "config.h"
-#include "consts.h"
+
 #include "common.h"
-#include <string.h>
+#include "consts.h"
+#include "redismodule.h"
+
 #include <assert.h>
+#include <string.h>
+#include "rmutil/strings.h"
+#include "rmutil/util.h"
 
 TSConfig TSGlobalConfig;
 
@@ -23,11 +25,14 @@ int ReadConfig(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
         const char *policy_cstr;
         size_t len;
 
-        if (RMUtil_ParseArgsAfter("COMPACTION_POLICY", argv, argc, "s", &policy) != REDISMODULE_OK) {
+        if (RMUtil_ParseArgsAfter("COMPACTION_POLICY", argv, argc, "s", &policy) !=
+            REDISMODULE_OK) {
             return TSDB_ERROR;
         }
         policy_cstr = RedisModule_StringPtrLen(policy, &len);
-        if (ParseCompactionPolicy(policy_cstr, &TSGlobalConfig.compactionRules, &TSGlobalConfig.compactionRulesCount) != TRUE ) {
+        if (ParseCompactionPolicy(policy_cstr,
+                                  &TSGlobalConfig.compactionRules,
+                                  &TSGlobalConfig.compactionRulesCount) != TRUE) {
             return TSDB_ERROR;
         }
 
@@ -36,24 +41,34 @@ int ReadConfig(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     }
 
     if (argc > 1 && RMUtil_ArgIndex("RETENTION_POLICY", argv, argc) >= 0) {
-        if (RMUtil_ParseArgsAfter("RETENTION_POLICY", argv, argc, "l", &TSGlobalConfig.retentionPolicy) != REDISMODULE_OK) {
+        if (RMUtil_ParseArgsAfter(
+                "RETENTION_POLICY", argv, argc, "l", &TSGlobalConfig.retentionPolicy) !=
+            REDISMODULE_OK) {
             return TSDB_ERROR;
         }
 
-        RedisModule_Log(ctx, "verbose", "loaded default retention policy: %lld \n", TSGlobalConfig.retentionPolicy);
+        RedisModule_Log(ctx,
+                        "verbose",
+                        "loaded default retention policy: %lld \n",
+                        TSGlobalConfig.retentionPolicy);
         TSGlobalConfig.hasGlobalConfig = TRUE;
     } else {
         TSGlobalConfig.retentionPolicy = RETENTION_TIME_DEFAULT;
     }
 
     if (argc > 1 && RMUtil_ArgIndex("MAX_SAMPLE_PER_CHUNK", argv, argc) >= 0) {
-        if (RMUtil_ParseArgsAfter("MAX_SAMPLE_PER_CHUNK", argv, argc, "l", &TSGlobalConfig.maxSamplesPerChunk) != REDISMODULE_OK) {
+        if (RMUtil_ParseArgsAfter(
+                "MAX_SAMPLE_PER_CHUNK", argv, argc, "l", &TSGlobalConfig.maxSamplesPerChunk) !=
+            REDISMODULE_OK) {
             return TSDB_ERROR;
         }
     } else {
         TSGlobalConfig.maxSamplesPerChunk = SAMPLES_PER_CHUNK_DEFAULT_SECS;
     }
-    RedisModule_Log(ctx, "verbose", "loaded default MAX_SAMPLE_PER_CHUNK policy: %lld \n", TSGlobalConfig.maxSamplesPerChunk);
+    RedisModule_Log(ctx,
+                    "verbose",
+                    "loaded default MAX_SAMPLE_PER_CHUNK policy: %lld \n",
+                    TSGlobalConfig.maxSamplesPerChunk);
     return TSDB_OK;
 }
 
@@ -71,53 +86,58 @@ int RTS_RlecPatchVersion;
 int RTS_RlecBuild;
 
 int RTS_CheckSupportedVestion() {
-  if (RTS_currVersion.redisMajorVersion < RTS_minSupportedVersion.redisMajorVersion) {
-    return REDISMODULE_ERR;
-  }
-
-  if (RTS_currVersion.redisMajorVersion == RTS_minSupportedVersion.redisMajorVersion) {
-    if (RTS_currVersion.redisMinorVersion < RTS_minSupportedVersion.redisMinorVersion) {
-      return REDISMODULE_ERR;
-    }
-
-    if (RTS_currVersion.redisMinorVersion == RTS_minSupportedVersion.redisMinorVersion) {
-      if (RTS_currVersion.redisPatchVersion < RTS_minSupportedVersion.redisPatchVersion) {
+    if (RTS_currVersion.redisMajorVersion < RTS_minSupportedVersion.redisMajorVersion) {
         return REDISMODULE_ERR;
-      }
     }
-  }
 
-  return REDISMODULE_OK;
+    if (RTS_currVersion.redisMajorVersion == RTS_minSupportedVersion.redisMajorVersion) {
+        if (RTS_currVersion.redisMinorVersion < RTS_minSupportedVersion.redisMinorVersion) {
+            return REDISMODULE_ERR;
+        }
+
+        if (RTS_currVersion.redisMinorVersion == RTS_minSupportedVersion.redisMinorVersion) {
+            if (RTS_currVersion.redisPatchVersion < RTS_minSupportedVersion.redisPatchVersion) {
+                return REDISMODULE_ERR;
+            }
+        }
+    }
+
+    return REDISMODULE_OK;
 }
 
 void RTS_GetRedisVersion() {
-  RedisModuleCtx *ctx = RedisModule_GetThreadSafeContext(NULL);
-  RedisModuleCallReply *reply = RedisModule_Call(ctx, "info", "c", "server");
-  assert(RedisModule_CallReplyType(reply) == REDISMODULE_REPLY_STRING);
-  size_t len;
-  const char *replyStr = RedisModule_CallReplyStringPtr(reply, &len);
+    RedisModuleCtx *ctx = RedisModule_GetThreadSafeContext(NULL);
+    RedisModuleCallReply *reply = RedisModule_Call(ctx, "info", "c", "server");
+    assert(RedisModule_CallReplyType(reply) == REDISMODULE_REPLY_STRING);
+    size_t len;
+    const char *replyStr = RedisModule_CallReplyStringPtr(reply, &len);
 
-  int n = sscanf(replyStr, "# Server\nredis_version:%d.%d.%d",
-                 &RTS_currVersion.redisMajorVersion, &RTS_currVersion.redisMinorVersion,
-                 &RTS_currVersion.redisPatchVersion);
-  if (n != 3) {
-    RedisModule_Log(NULL, "warning", "Could not extract redis version");
-  }
-
-  RTS_RlecMajorVersion = -1;
-  RTS_RlecMinorVersion = -1;
-  RTS_RlecPatchVersion = -1;
-  RTS_RlecBuild = -1;
-  const char *enterpriseStr = strstr(replyStr, "rlec_version:");
-  if (enterpriseStr) {
-    n = sscanf(enterpriseStr, "rlec_version:%d.%d.%d-%d",
-               &RTS_RlecMajorVersion, &RTS_RlecMinorVersion,
-               &RTS_RlecPatchVersion, &RTS_RlecBuild);
-    if (n != 4) {
-      RedisModule_Log(NULL, "warning", "Could not extract enterprise version");
+    int n = sscanf(replyStr,
+                   "# Server\nredis_version:%d.%d.%d",
+                   &RTS_currVersion.redisMajorVersion,
+                   &RTS_currVersion.redisMinorVersion,
+                   &RTS_currVersion.redisPatchVersion);
+    if (n != 3) {
+        RedisModule_Log(NULL, "warning", "Could not extract redis version");
     }
-  }
 
-  RedisModule_FreeCallReply(reply);
-  RedisModule_FreeThreadSafeContext(ctx);
+    RTS_RlecMajorVersion = -1;
+    RTS_RlecMinorVersion = -1;
+    RTS_RlecPatchVersion = -1;
+    RTS_RlecBuild = -1;
+    const char *enterpriseStr = strstr(replyStr, "rlec_version:");
+    if (enterpriseStr) {
+        n = sscanf(enterpriseStr,
+                   "rlec_version:%d.%d.%d-%d",
+                   &RTS_RlecMajorVersion,
+                   &RTS_RlecMinorVersion,
+                   &RTS_RlecPatchVersion,
+                   &RTS_RlecBuild);
+        if (n != 4) {
+            RedisModule_Log(NULL, "warning", "Could not extract enterprise version");
+        }
+    }
+
+    RedisModule_FreeCallReply(reply);
+    RedisModule_FreeThreadSafeContext(ctx);
 }
