1749fb1e9a88a340be96dae76e56deeba582f4ac
ariel shtul <ashtul@gmail.com>
2020-06-09 12:22:13 +0300

    fix lint

--- src/compaction.c
+++ src/compaction.c
@@ -1,3 +1,4 @@
+/*
  * Copyright 2018-2019 Redis Labs Ltd. and Contributors
  *
  * This file is available under the Redis Labs Source Available License Agreement
@@ -6,8 +7,8 @@
 
 #include <ctype.h>
 #include <math.h> // sqrt
-#include <rmutil/alloc.h>
 #include <string.h>
+#include <rmutil/alloc.h>
 
 typedef struct MaxMinContext
 {
