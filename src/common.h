12c936eb8c03cd0f5e890a45d639b75e219905e8
Danni Moiseyev <danni@redislabs.com>
2020-07-06 21:53:15 +0300

    rename `ERR` to `RTS_ERR`

--- src/common.h
+++ src/common.h
@@ -1,6 +1,6 @@
 
 #pragma once
 
-#define ERR "ERR"
+#define RTS_ERR "ERR"
 #define RTS_ReplyError(ctx, err_type, msg) RedisModule_ReplyWithError(ctx, err_type " " msg);
-#define RTS_ReplyGeneralError(ctx, msg) RTS_ReplyError(ctx, ERR, msg);
+#define RTS_ReplyGeneralError(ctx, msg) RTS_ReplyError(ctx, RTS_ERR, msg);
