bb63043da8f4b5755bc4560b2781b931510b410b
Ariel Shtul <ariel.shtul@redislabs.com>
2020-07-13 15:09:11 +0300

    Fix for when retention is larger than timestamp (#462)
    * Fix for when retention is larger than timestamp
    
    * per review
    
    (cherry picked from commit cf9500cb77ec16e344d17b2408b25667d143441f)

--- src/module.c
+++ src/module.c
@@ -700,7 +700,7 @@ static int internalAdd(RedisModuleCtx *ctx,
     timestamp_t lastTS = series->lastTimestamp;
     uint64_t retention = series->retentionTime;
     // ensure inside retention period.
-    if (retention && timestamp < lastTS && timestamp < lastTS - retention) {
+    if (retention && timestamp < lastTS && retention < lastTS - timestamp) {
         RTS_ReplyGeneralError(ctx, "TSDB: Timestamp is older than retention");
         return REDISMODULE_ERR;
     }
