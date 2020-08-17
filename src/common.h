9cd554168f13a4269c2757641b8d616adb1c745b
filipe oliveira <filipecosta.90@gmail.com>
2020-08-17 13:01:03 +0100

    fix: Error messages must contain all the error, including the initial error code (ERR) (#440)
    * fix: Error messages must contain all the error, including the initial error code (ERR)
    
    * use macro for Replying error
    
    * rename `ERR` to `RTS_ERR`
    
    Co-authored-by: Danni Moiseyev <danni@redislabs.com>
    Co-authored-by: Ariel Shtul <ariel.shtul@redislabs.com>
    (cherry picked from commit 7b894943c67e27de666157b4e25e9d0a083c5920)

--- src/common.h
+++ src/common.h
@@ -1,2 +1,6 @@
 
 #pragma once
+
+#define RTS_ERR "ERR"
+#define RTS_ReplyError(ctx, err_type, msg) RedisModule_ReplyWithError(ctx, err_type " " msg);
+#define RTS_ReplyGeneralError(ctx, msg) RTS_ReplyError(ctx, RTS_ERR, msg);
