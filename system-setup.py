cce6625fbaff0c5168a8537ccfc53948f5483017
Rafi Einstein <raffapen@outlook.com>
2020-10-01 13:53:56 +0300

    Updated readies (#545)
    (cherry picked from commit 8fdac0d40737c52ae74c255d8753126087e63f68)

--- system-setup.py
+++ system-setup.py
@@ -30,7 +30,7 @@ class RedisTimeSeriesSetup(paella.Setup):
     def fedora(self):
         self.group_install("'Development Tools'")
 
-    def macosx(self):
+    def macos(self):
         if sh('xcode-select -p') == '':
             fatal("Xcode tools are not installed. Please run xcode-select --install.")
         self.install("redis")
