8d1ecbe399e30498f061a5e8f18eb965d75542f5
Danni Moiseyev <danni@redislabs.com>
2020-11-02 16:34:00 +0200

    duplicate policy: add SUM option (#565)
    * duplicate policy: add SUM option
    
    Setting SUM as a duplicate policy will result in the value for a
    single time window being the sum of all data written in the time
    window. This is useful for high-frequency event counting that can't
    guarantee two samples won't be written in the same time window
    
    * add unittests to test DUPLICATE_POLICY parsing
    
    Co-authored-by: Dan Pipe-Mazo <dpipemazo@gmail.com>

---
+++ src/unittests_parse_duplicate_policy.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+#include "consts.h"
+#include "generic_chunk.h"
+#include "minunit.h"
+
+
+#define DuplicatePolicyFromCString(str) DuplicatePolicyFromString(str, strlen(str))
+
+MU_TEST(test_duplicate_policy_parse) {
+    mu_check(DuplicatePolicyFromCString("Min") == DP_MIN);
+    mu_check(DuplicatePolicyFromCString("MAX") == DP_MAX);
+    mu_check(DuplicatePolicyFromCString("sum") == DP_SUM);
+    mu_check(DuplicatePolicyFromCString("Last") == DP_LAST);
+    mu_check(DuplicatePolicyFromCString("Block") == DP_BLOCK);
+    mu_check(DuplicatePolicyFromCString("first") == DP_FIRST);
+    mu_check(DuplicatePolicyFromCString("DSADASD") == DP_INVALID);
+}
+
+MU_TEST(test_duplicate_policy_to_string) {
+    mu_check(strcmp(DuplicatePolicyToString(DP_MIN), "min") == 0);
+}
+
+MU_TEST_SUITE(parse_duplicate_policy_test_suite) {
+        MU_RUN_TEST(test_duplicate_policy_parse);
+        MU_RUN_TEST(test_duplicate_policy_to_string);
+}
