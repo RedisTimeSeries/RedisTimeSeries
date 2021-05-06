f0f1660e9f5c998386a961a31ec197f8d4e4fcce
Guy Korland <gkorland@gmail.com>
2021-05-06 09:48:08 +0300

    fix tests

--- tests/flow/test_rename.py
+++ tests/flow/test_rename.py
@@ -1,50 +1,49 @@
 from test_helper_classes import TSInfo
 
 def test_rename_src(env):
+    with env.getClusterConnectionIfNeeded() as r:
 
-    assert env.cmd('TS.CREATE', 'a1{1}')
-    assert env.cmd('TS.CREATE', 'b{1}')
+        assert r.execute_command('TS.CREATE', 'a1{1}')
+        assert r.execute_command('TS.CREATE', 'b{1}')
 
-    env.assertTrue(env.cmd('RENAME', 'a1{1}', 'a2{1}'))
-    aInfo = TSInfo(env.cmd('TS.INFO', 'a2{1}'))
-    env.assertEqual(aInfo.sourceKey, None)
-    env.assertEqual(aInfo.rules, [])
-    
-    assert env.cmd('TS.CREATERULE', 'a2{1}', 'b{1}', 'AGGREGATION', 'AVG', 5000)
-    bInfo = TSInfo(env.cmd('TS.INFO', 'b{1}'))
-    env.assertEqual(bInfo.sourceKey, b'a2{1}')
-    env.assertEqual(bInfo.rules, [])
+        env.assertTrue(r.execute_command('RENAME', 'a1{1}', 'a2{1}'))
+        aInfo = TSInfo(r.execute_command('TS.INFO', 'a2{1}'))
+        env.assertEqual(aInfo.sourceKey, None)
+        env.assertEqual(aInfo.rules, [])
+        
+        assert r.execute_command('TS.CREATERULE', 'a2{1}', 'b{1}', 'AGGREGATION', 'AVG', 5000)
+        bInfo = TSInfo(r.execute_command('TS.INFO', 'b{1}'))
+        env.assertEqual(bInfo.sourceKey, b'a2{1}')
+        env.assertEqual(bInfo.rules, [])
 
-    env.assertTrue(env.cmd('RENAME', 'a2{1}', 'a3{1}'))
-    bInfo = TSInfo(env.cmd('TS.INFO', 'b{1}'))
-    env.assertEqual(bInfo.sourceKey, b'a3{1}')
-    env.assertEqual(bInfo.rules, [])
+        env.assertTrue(r.execute_command('RENAME', 'a2{1}', 'a3{1}'))
+        bInfo = TSInfo(r.execute_command('TS.INFO', 'b{1}'))
+        env.assertEqual(bInfo.sourceKey, b'a3{1}')
+        env.assertEqual(bInfo.rules, [])
 
 
 def test_rename_dst(env):
 
-    assert env.cmd('TS.CREATE', 'a{2}')
-    assert env.cmd('TS.CREATE', 'b{2}')
-    assert env.cmd('TS.CREATERULE', 'a{2}', 'b{2}', 'AGGREGATION', 'AVG', 5000)
+    with env.getClusterConnectionIfNeeded() as r:
 
-    env.assertTrue(env.cmd('RENAME', 'b{2}', 'b1{2}'))
-    aInfo = TSInfo(env.cmd('TS.INFO', 'a{2}'))
-    env.assertEqual(aInfo.sourceKey, None)
-    env.assertEqual(aInfo.rules[0][0], b'b1{2}')
+        assert r.execute_command('TS.CREATE', 'a{2}')
+        assert r.execute_command('TS.CREATE', 'b{2}')
+        assert r.execute_command('TS.CREATERULE', 'a{2}', 'b{2}', 'AGGREGATION', 'AVG', 5000)
 
-    assert env.cmd('TS.CREATE', 'c{2}')
-    assert env.cmd('TS.CREATERULE', 'a{2}', 'c{2}', 'AGGREGATION', 'COUNT', 2000)
+        env.assertTrue(r.execute_command('RENAME', 'b{2}', 'b1{2}'))
+        aInfo = TSInfo(r.execute_command('TS.INFO', 'a{2}'))
+        env.assertEqual(aInfo.sourceKey, None)
+        env.assertEqual(aInfo.rules[0][0], b'b1{2}')
 
-    assert env.cmd('TS.CREATE', 'd{2}')
-    assert env.cmd('TS.CREATERULE', 'a{2}', 'd{2}', 'AGGREGATION', 'SUM', 3000)
+        assert r.execute_command('TS.CREATE', 'c{2}')
+        assert r.execute_command('TS.CREATERULE', 'a{2}', 'c{2}', 'AGGREGATION', 'COUNT', 2000)
 
-    env.assertTrue(env.cmd('RENAME', 'c{2}', 'c1{2}'))
-    aInfo = TSInfo(env.cmd('TS.INFO', 'a{2}'))
-    env.assertEqual(aInfo.sourceKey, None)
-    env.assertEqual(aInfo.rules[0][0], b'b1{2}')
-    env.assertEqual(aInfo.rules[1][0], b'c1{2}')
-    env.assertEqual(aInfo.rules[2][0], b'd{2}')
+        assert r.execute_command('TS.CREATE', 'd{2}')
+        assert r.execute_command('TS.CREATERULE', 'a{2}', 'd{2}', 'AGGREGATION', 'SUM', 3000)
 
-
-
-
+        env.assertTrue(r.execute_command('RENAME', 'c{2}', 'c1{2}'))
+        aInfo = TSInfo(r.execute_command('TS.INFO', 'a{2}'))
+        env.assertEqual(aInfo.sourceKey, None)
+        env.assertEqual(aInfo.rules[0][0], b'b1{2}')
+        env.assertEqual(aInfo.rules[1][0], b'c1{2}')
+        env.assertEqual(aInfo.rules[2][0], b'd{2}')
