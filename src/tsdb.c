595fe13c579c90cd54a69310983425af4cae8e47
Guy Korland <gkorland@gmail.com>
2021-05-09 14:43:36 +0300

    Only close key that we managed to open

--- src/tsdb.c
+++ src/tsdb.c
@@ -256,13 +256,13 @@ void RenameSeriesTo(RedisModuleCtx *ctx, RedisModuleString *keyTo) {
                                 "warning",
                                 "couldn't open key or key is not a Timeseries. key=%s",
                                 destKeyName);
+            } else {
+                // rename the srcKey in the destKey
+                RedisModule_FreeString(NULL, destSeries->srcKey);
+                destSeries->srcKey = RedisModule_RetainString(NULL, keyTo);
+
+                RedisModule_CloseKey(destKey);
             }
-
-            // rename the srcKey in the destKey
-            RedisModule_FreeString(NULL, destSeries->srcKey);
-            destSeries->srcKey = RedisModule_HoldString(NULL, keyTo);
-
-            RedisModule_CloseKey(destKey);
             rule = rule->nextRule;
         }
     }
