5be476d092df6d8a066b3aa4335079674cdf1180
Chayim I. Kirshen <c@kirshen.com>
2021-07-01 17:37:12 +0300

    mac

--- system-setup.py
+++ system-setup.py
@@ -44,6 +44,9 @@ class RedisTimeSeriesSetup(paella.Setup):
         self.run("%s/bin/getgcc" % READIES)
         self.install("python3-networkx")
 
+    def macos(self):
+        self.install_gnu_utils()
+
     def common_last(self):
         if self.dist == "centos" and self.ver == "8":
             self.install("https://pkgs.dyn.su/el8/base/x86_64/lcov-1.14-3.el8.noarch.rpm")
