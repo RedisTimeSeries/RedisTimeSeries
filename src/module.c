5e33597e8ca3b157fb289a732874656537aaabfe
Guy Korland <gkorland@gmail.com>
2021-07-26 17:13:46 +0300

    Verify args before we open the key

--- src/module.c
+++ src/module.c
@@ -1013,15 +1013,15 @@ int TSDB_delete(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
         return RedisModule_WrongArity(ctx);
     }
 
+    RangeArgs args = { 0 };
+    if (parseRangeArguments(ctx, 2, argv, argc, (timestamp_t)0, &args) != REDISMODULE_OK) {
+        return REDISMODULE_ERR;
+    }
+
     Series *series;
     RedisModuleKey *key;
     const int status = GetSeries(ctx, argv[1], &key, &series, REDISMODULE_READ);
     if (!status) {
-        return REDISMODULE_ERR;
-    }
-
-    RangeArgs args = { 0 };
-    if (parseRangeArguments(ctx, 2, argv, argc, (timestamp_t)0, &args) != REDISMODULE_OK) {
         return REDISMODULE_ERR;
     }
 
