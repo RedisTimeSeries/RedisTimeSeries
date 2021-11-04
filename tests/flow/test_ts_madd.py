560e9dd406d44f70d96db9bb623edb93928b5590
Danni Moiseyev <danni@redislabs.com>
2021-11-04 22:02:18 +0200

    add AOF testing

--- tests/flow/test_ts_madd.py
+++ tests/flow/test_ts_madd.py
@@ -1,7 +1,8 @@
+import os
 import time
+import aof_parser
 
-from RLTest import Env
-
+from RLTest import Env, StandardEnv
 
 def test_madd():
     sample_len = 1024
@@ -114,3 +115,11 @@ def test_madd_some_failed_replicas():
 
     with env.getSlaveConnection() as r:
         env.assertEqual(r.execute_command("ts.range", "test_key1", "-", "+"),  [[122, b'11'], [123, b'11'], [124, b'12'], [125, b'12']])
+
+    if env.useAof and isinstance(env.envRunner, StandardEnv):
+        cmds = aof_parser.parse_file(os.path.join(env.envRunner.dbDirPath, env.envRunner._getFileName('master', '.aof')))
+        cmds = filter(lambda c: c[0].lower().startswith('ts.'), cmds)
+        env.assertEqual(list(cmds),
+                        [['ts.create', 'test_key1', 'DUPLICATE_POLICY', 'block'],
+                         ['TS.MADD', 'test_key1', '123', '11', 'test_key1', '124', '12'],
+                         ['TS.MADD', 'test_key1', '122', '11', 'test_key1', '125', '12']])
