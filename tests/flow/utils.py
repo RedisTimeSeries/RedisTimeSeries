585618f36e2e17fb1838367d81c1b3c189f570ac
rafie <rafi@redislabs.com>
2021-11-21 13:10:01 +0200

    fixes 6

--- tests/flow/utils.py
+++ tests/flow/utils.py
@@ -1,21 +1,29 @@
 import inspect
 
 from RLTest import Env as rltestEnv
+from includes import *
 
 def Env(*args, **kwargs):
     if 'testName' not in kwargs:
         kwargs['testName'] = '%s.%s' % (inspect.getmodule(inspect.currentframe().f_back).__name__, inspect.currentframe().f_back.f_code.co_name)
     env = rltestEnv(*args, **kwargs)
-    for shard in range(0, env.shardsCount):
-        modules = env.getConnection(shard).execute_command('MODULE', 'LIST')
-        if not any(module for module in modules if (module[1] == b'timeseries' or module[1] == 'timeseries')):
-            break
-        env.getConnection(shard).execute_command('timeseries.REFRESHCLUSTER')
+    if not RLEC_CLUSTER:
+        for shard in range(0, env.shardsCount):
+            modules = env.getConnection(shard).execute_command('MODULE', 'LIST')
+            if not any(module for module in modules if (module[1] == b'timeseries' or module[1] == 'timeseries')):
+                break
+            env.getConnection(shard).execute_command('timeseries.REFRESHCLUSTER')
+    else:
+        for shard in range(0, env.shardsCount):
+            try:
+                env.getConnection(shard).execute_command('timeseries.REFRESHCLUSTER')
+            except:
+                pass
     return env
 
 def set_hertz(env):
+    if RLEC_CLUSTER:
+        return
     for shard in range(0, env.shardsCount):
         # Lower hz value to make it more likely that mrange triggers key expiration
         assert env.getConnection(shard).execute_command('config set hz 1') == b'OK'
-
-
