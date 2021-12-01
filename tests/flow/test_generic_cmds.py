277d3686ffdabb287948f8eb8961cc92d25ac92f
Danni Moiseyev <danni@redislabs.com>
2021-12-01 12:58:21 +0200

    adjust test_generic_cmds to work on a cluster

--- tests/flow/test_generic_cmds.py
+++ tests/flow/test_generic_cmds.py
@@ -2,38 +2,38 @@ import pytest
 import redis
 import time
 
-from RLTest import Env
+from packaging import version
+from includes import *
+from utils import Env
 
 def init(env, r, compression="COMPRESSED"):
-    assert r.execute_command('TS.CREATE', 't1', 'ENCODING', compression, 'LABELS', 'name', 'mush', 'fname', 'ox')
-    assert r.execute_command('TS.CREATE', 't2', 'ENCODING', compression, 'LABELS', 'name', 'zavi', 'fname', 'zav')
-    assert r.execute_command('TS.CREATE', 't3', 'ENCODING', compression, 'LABELS', 'name', 'rex', 'fname', 'dog')
-    assert r.execute_command('TS.CREATERULE', 't1', 't3', 'AGGREGATION', 'avg', '10')
+    assert r.execute_command('TS.CREATE', 't{1}', 'ENCODING', compression, 'LABELS', 'name', 'mush', 'fname', 'ox')
+    assert r.execute_command('TS.CREATE', 't{2}', 'ENCODING', compression, 'LABELS', 'name', 'zavi', 'fname', 'zav')
+    assert r.execute_command('TS.CREATE', 't{1}_agg', 'ENCODING', compression, 'LABELS', 'name', 'rex', 'fname', 'dog')
+    assert r.execute_command('TS.CREATERULE', 't{1}', 't{1}_agg', 'AGGREGATION', 'avg', '10')
     
-    assert r.execute_command('TS.add', 't1', '10', '19')
+    assert r.execute_command('TS.add', 't{1}', '10', '19')
 
     res = r.execute_command('TS.QUERYINDEX', 'name=mush')
-    env.assertEqual(res[0], b't1')
+    env.assertEqual(res[0], b't{1}')
 
     res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
-    env.assertEqual(sorted(res), [b't1', b't2', b't3'])
+    env.assertEqual(sorted(res), sorted([b't{1}', b't{2}', b't{1}_agg']))
 
 def test_del():
     env = Env()
-    env.skipOnCluster()
     with env.getClusterConnectionIfNeeded() as r:
         init(env, r)
         
-        assert r.execute_command('del t1')
+        assert r.execute_command('del', 't{1}')
 
         res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
-        env.assertEqual(sorted(res), [b't2', b't3'])
-        res = r.execute_command('keys *')
-        env.assertEqual(sorted(res), [b't2', b't3'])
+        env.assertEqual(sorted(res), sorted([b't{2}', b't{1}_agg']))
+        res = r.execute_command('TS.MGET', 'filter',  'name=(mush,zavi,rex)')
+        env.assertEqual(sorted(res), sorted([[b't{2}', [], []], [b't{1}_agg', [], []]]))
 
 def test_flush():
     env = Env()
-    env.skipOnCluster()
     with env.getClusterConnectionIfNeeded() as r:
         init(env, r)
 
@@ -41,31 +41,28 @@ def test_flush():
 
         res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
         env.assertEqual(res, [])
-        res = r.execute_command('keys *')
-        env.assertEqual(res, [])
 
         init(env, r)
         assert r.execute_command('FLUSHDB')
         res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
         env.assertEqual(res, [])
-        res = r.execute_command('keys *')
-        env.assertEqual(res, [])
 
 def test_set():
     env = Env()
-    env.skipOnCluster()
     with env.getClusterConnectionIfNeeded() as r:
         init(env, r)
 
-        assert r.execute_command('SET', 't1', 'awesome')
+        assert r.execute_command('SET', 't{1}', 'awesome')
         res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
-        env.assertEqual(sorted(res), [b't2', b't3'])
-        res = r.execute_command('keys *')
-        env.assertEqual(sorted(res), [b't1', b't2', b't3'])
+        env.assertEqual(sorted(res), sorted([b't{2}', b't{1}_agg']))
+        env.assertEqual(r.execute_command('GET', 't{1}'), b'awesome')
+        res = r.execute_command('TS.MGET', 'filter', 'name=(mush,zavi,rex)')
+        env.assertEqual(sorted(res), sorted([[b't{2}', [], []], [b't{1}_agg', [], []]]))
 
 def test_evict():
     env = Env()
     env.skipOnCluster()
+    skip_on_rlec()
     with env.getClusterConnectionIfNeeded() as r:
         info = r.execute_command('INFO')
         max_mem = info['used_memory'] + 1024*1024
@@ -73,10 +70,10 @@ def test_evict():
         assert r.execute_command('CONFIG', 'SET', 'maxmemory-policy', 'allkeys-lru')
         init(env, r)
 
-        # make sure t1 deleted
+        # make sure t{1} deleted
         res = r.execute_command('keys *')
         i = 4
-        while(sorted(res)[0] == b't1'):
+        while(sorted(res)[0] == b't{1}'):
             assert r.execute_command('TS.CREATE', 't%s' % (i))
             i += 1
             res = r.execute_command('keys *')
@@ -90,73 +87,75 @@ def test_evict():
 
 def test_expire():
     env = Env()
-    env.skipOnCluster()
     with env.getClusterConnectionIfNeeded() as r:
         init(env, r)
         
-        res = r.execute_command('EXPIRE', 't1', 1)
+        res = r.execute_command('EXPIRE', 't{1}', 1)
         time.sleep(2)
 
         res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
 
-        env.assertEqual(sorted(res), [b't2', b't3'])
-        res = r.execute_command('keys *')
-        env.assertEqual(sorted(res), [b't2', b't3'])
+        env.assertEqual(sorted(res), sorted([b't{2}', b't{1}_agg']))
+        res = r.execute_command('TS.MGET', 'filter', 'name=(mush,zavi,rex)')
+        env.assertEqual(sorted(res), sorted([[b't{2}', [], []], [b't{1}_agg', [], []]]))
 
 
 def test_unlink():
     env = Env()
-    env.skipOnCluster()
     with env.getClusterConnectionIfNeeded() as r:
         init(env, r)
         
-        res = r.execute_command('UNLINK', 't1')
-
+        res = r.execute_command('UNLINK', 't{1}')
         res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
-        env.assertEqual(sorted(res), [b't2', b't3'])
-        res = r.execute_command('keys *')
-        env.assertEqual(sorted(res), [b't2', b't3'])
+        env.assertEqual(sorted(res), sorted([b't{2}', b't{1}_agg']))
+        res = r.execute_command('TS.MGET', 'filter', 'name=(mush,zavi,rex)')
+        env.assertEqual(sorted(res), sorted([[b't{2}', [], []], [b't{1}_agg', [], []]]))
 
 def test_restore():
     env = Env()
-    env.skipOnCluster()
     with env.getClusterConnectionIfNeeded() as r:
         init(env, r)
         
-        serialized_val = r.execute_command('DUMP', 't1')
-        assert r.execute_command('del t1')
+        serialized_val = r.execute_command('DUMP', 't{1}')
+        assert r.execute_command('del', 't{1}')
 
         res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
-        env.assertEqual(sorted(res), [b't2', b't3'])
-        res = r.execute_command('keys *')
-        env.assertEqual(sorted(res), [b't2', b't3'])
+        env.assertEqual(sorted(res), sorted({b't{2}', b't{1}_agg'}))
+        res = r.execute_command('TS.MGET', 'filter', 'name=(mush,zavi,rex)')
+        env.assertEqual(sorted(res), sorted([[b't{1}_agg', [], []],
+                                              [b't{2}', [], []]
+                                              ]))
 
-        assert r.execute_command('RESTORE', 't1', '0', serialized_val)
+        assert r.execute_command('RESTORE', 't{1}', '0', serialized_val)
         res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
-        env.assertEqual(sorted(res), [b't1',b't2', b't3'])
-        res = r.execute_command('keys *')
-        env.assertEqual(sorted(res), [b't1', b't2', b't3'])
+        env.assertEqual(sorted(res), sorted([b't{1}', b't{2}', b't{1}_agg']))
+        res = r.execute_command('TS.MGET', 'filter', 'name=(mush,zavi,rex)')
+        env.assertEqual(sorted(res), sorted([[b't{1}', [], [10, b'19']],
+                                              [b't{2}', [], []],
+                                              [b't{1}_agg', [], []]
+                                              ]))
 
 def test_rename():
     env = Env()
-    env.skipOnCluster()
     with env.getClusterConnectionIfNeeded() as r:
         init(env, r)
         
-        assert r.execute_command('RENAME', 't1', 't4')
-        res = r.execute_command('ts.info', 't3')
+        assert r.execute_command('RENAME', 't{1}', 't{1}_renamed')
+        res = r.execute_command('ts.info', 't{1}_agg')
         index = res.index(b'sourceKey')
-        env.assertEqual(res[index+1], b't4')
+        env.assertEqual(res[index+1], b't{1}_renamed')
 
         res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
-        env.assertEqual(sorted(res), [b't2', b't3', b't4'])
-        res = r.execute_command('keys *')
-        env.assertEqual(sorted(res), [b't2', b't3', b't4'])
+        env.assertEqual(sorted(res), sorted([b't{2}', b't{1}_agg', b't{1}_renamed']))
+        res = r.execute_command('TS.MGET', 'filter',  'name=(mush,zavi,rex)')
+        env.assertEqual(sorted(res), sorted([[b't{2}', [], []],
+                                             [b't{1}_agg', [], []],
+                                             [b't{1}_renamed', [],  [10, b'19']]]))
 
-        assert r.execute_command('RENAME', 't3', 't1')
-        res = r.execute_command('ts.info', 't4')
+        assert r.execute_command('RENAME', 't{1}_agg', 't{1}')
+        res = r.execute_command('ts.info', 't{1}_renamed')
         index = res.index(b'rules')
-        env.assertEqual(res[index+1], [[b't1', 10, b'AVG']])
+        env.assertEqual(res[index+1], [[b't{1}', 10, b'AVG']])
 
 
 def test_renamenx():
@@ -188,71 +187,76 @@ def test_copy_compressed():
     with env.getClusterConnectionIfNeeded() as r:
         init(env, r)
         
-        assert r.execute_command('COPY', 't1', 't4')
-        res = r.execute_command('ts.info', 't3')
+        assert r.execute_command('RENAMENX', 't{1}', 't{1}_renamed')
+        res = r.execute_command('ts.info', 't{1}_agg')
         index = res.index(b'sourceKey')
-        env.assertEqual(res[index+1], b't1')
-
-        res = r.execute_command('ts.info', 't4')
-        index = res.index(b'sourceKey')
-        env.assertEqual(res[index+1], None)
-        index = res.index(b'rules')
-        env.assertEqual(res[index+1], [])
-
-        res = r.execute_command('TS.get', 't4')
-        assert res == [10, b'19']
+        env.assertEqual(res[index+1], b't{1}_renamed')
 
         res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
-        env.assertEqual(sorted(res), [b't1', b't2', b't3', b't4'])
-        res = r.execute_command('keys *')
-        env.assertEqual(sorted(res), [b't1', b't2', b't3', b't4'])
+        env.assertEqual(sorted(res), sorted([b't{2}', b't{1}_agg', b't{1}_renamed']))
+        res = r.execute_command('TS.MGET', 'filter', 'name=(mush,zavi,rex)')
+        env.assertEqual(sorted(res), sorted([[b't{2}', [], []],
+                                             [b't{1}_agg', [], []],
+                                             [b't{1}_renamed',  [], [10, b'19']]]))
 
-        assert r.execute_command('COPY', 't3', 't5')
-        res = r.execute_command('ts.info', 't5')
-        index = res.index(b'sourceKey')
-        env.assertEqual(res[index+1], None)
+        assert r.execute_command('RENAMENX', 't{1}_agg', 't{1}')
+        res = r.execute_command('ts.info', 't{1}_renamed')
         index = res.index(b'rules')
-        env.assertEqual(res[index+1], [])
+        env.assertEqual(res[index+1], [[b't{1}', 10, b'AVG']])
 
-        res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
-        env.assertEqual(sorted(res), [b't1', b't2', b't3', b't4', b't5'])
-        res = r.execute_command('keys *')
-        env.assertEqual(sorted(res), [b't1', b't2', b't3', b't4', b't5'])
-
-def test_copy_uncompressed():
+def test_copy_compressed_uncompressed():
     env = Env()
     env.skipOnCluster()
     env.skipOnVersionSmaller("7.0.0")
-    with env.getClusterConnectionIfNeeded() as r:
-        init(env, r, "UNCOMPRESSED")
-        
-        assert r.execute_command('COPY', 't1', 't4')
-        res = r.execute_command('ts.info', 't3')
-        index = res.index(b'sourceKey')
-        env.assertEqual(res[index+1], b't1')
+    for compresssion in [ "UNCOMPRESSED", 'COMPRESSED']:
+        with env.getClusterConnectionIfNeeded() as r:
+            res = r.execute_command('INFO')
+            if(version.parse(res['redis_version']) < version.parse("6.0.0")):
+                self.skip() # copy exists only from version 6
+            r.execute_command('FLUSHALL')
+            init(env, r, compression=compresssion)
+            for i in range(1000):
+                assert r.execute_command('TS.ADD', 't{1}', 1638304650 + i, i)
 
-        res = r.execute_command('ts.info', 't4')
-        index = res.index(b'sourceKey')
-        env.assertEqual(res[index+1], None)
-        index = res.index(b'rules')
-        env.assertEqual(res[index+1], [])
+            assert r.execute_command('COPY', 't{1}', 't{1}_copied')
+            t1_info = r.execute_command('ts.info', 't{1}')
+            res = r.execute_command('ts.info', 't{1}_agg')
+            index = res.index(b'sourceKey')
+            env.assertEqual(res[index+1], b't{1}')
 
-        res = r.execute_command('TS.get', 't4')
-        assert res == [10, b'19']
+            res = r.execute_command('ts.info', 't{1}_copied')
+            index = res.index(b'sourceKey')
+            env.assertEqual(res[index+1], None)
+            index = res.index(b'rules')
+            env.assertEqual(res[index+1], [])
+            index = res.index(b'labels')
+            env.assertEqual(res[index+1], t1_info[index+1])
 
-        res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
-        env.assertEqual(sorted(res), [b't1', b't2', b't3', b't4'])
-        res = r.execute_command('keys *')
-        env.assertEqual(sorted(res), [b't1', b't2', b't3', b't4'])
+            copied_data = r.execute_command('TS.range', 't{1}', '-', '+')
+            data = r.execute_command('TS.range', 't{1}_copied', '-', '+',)
+            assert data == copied_data
 
-        assert r.execute_command('COPY', 't3', 't5')
-        res = r.execute_command('ts.info', 't5')
-        index = res.index(b'sourceKey')
-        env.assertEqual(res[index+1], None)
-        index = res.index(b'rules')
-        env.assertEqual(res[index+1], [])
+            res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
+            env.assertEqual(sorted(res), sorted([b't{1}', b't{2}', b't{1}_agg', b't{1}_copied']))
+            res = r.execute_command('TS.MGET', 'filter', 'name=(mush,zavi,rex)')
+            env.assertEqual(sorted(res), sorted([[b't{1}', [], [1638305649, b'999']],
+                                                 [b't{2}', [], []],
+                                                 [b't{1}_agg', [],  [1638305630, b'984.5']],
+                                                 [b't{1}_copied', [], [1638305649, b'999']]]))
 
-        res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
-        env.assertEqual(sorted(res), [b't1', b't2', b't3', b't4', b't5'])
-        res = r.execute_command('keys *')
-        env.assertEqual(sorted(res), [b't1', b't2', b't3', b't4', b't5'])
+            assert r.execute_command('COPY', 't{1}_agg', 't{1}_agg_copied')
+            res = r.execute_command('ts.info', 't{1}_agg_copied')
+            index = res.index(b'sourceKey')
+            env.assertEqual(res[index+1], None)
+            index = res.index(b'rules')
+            env.assertEqual(res[index+1], [])
+
+            res = r.execute_command('TS.QUERYINDEX', 'name=(mush,zavi,rex)')
+            env.assertEqual(sorted(res), sorted([b't{1}', b't{2}', b't{1}_agg', b't{1}_copied', b't{1}_agg_copied']))
+            res = r.execute_command('TS.MGET', 'filter', 'name=(mush,zavi,rex)')
+            env.assertEqual(sorted(res), sorted([[b't{1}', [], [1638305649, b'999']],
+                                                 [b't{2}', [], []],
+                                                 [b't{1}_agg', [],  [1638305630, b'984.5']],
+                                                 [b't{1}_copied', [], [1638305649, b'999']],
+                                                 [b't{1}_agg_copied', [], [1638305630, b'984.5']]]
+                                                ))
