afe2250098c346b61428d0256b0204259154dc08
Ofir Moskovich <ofir.moskovich@redislabs.com>
2022-02-16 18:49:09 +0200

    max avx512f indexing fix

--- src/compactions/compaction_avx512f.c
+++ src/compactions/compaction_avx512f.c
@@ -5,15 +5,12 @@ void MaxAppendValuesAVX512F(void *__restrict__ context,
                             double *__restrict__ values,
                             size_t si,
                             size_t ei) {
-    if (ei - si + 1 > VECTOR_SIZE * 3) {
+    if ((ei - si + 1) < VECTOR_SIZE * 2) {
         MaxAppendValuesVec(context, values, si, ei);
         return;
     }
+
     double *res = &((MaxMinContext *)context)->maxValue;
-    double vec[VECTOR_SIZE] __attribute__((aligned(CACHE_LINE_SIZE))) = {
-        _DOUBLE_MOST_NEG, _DOUBLE_MOST_NEG, _DOUBLE_MOST_NEG, _DOUBLE_MOST_NEG,
-        _DOUBLE_MOST_NEG, _DOUBLE_MOST_NEG, _DOUBLE_MOST_NEG, _DOUBLE_MOST_NEG
-    };
 
     // search in beginning
     while (si <= ei && !is_aligned(&values[si], CACHE_LINE_SIZE)) {
@@ -25,21 +22,27 @@ void MaxAppendValuesAVX512F(void *__restrict__ context,
         return;
     }
 
-    __m512d res_avx = _mm512_load_pd(vec);
-    __m512d values_avx;
-    size_t vec_ei = (ei - si < VECTOR_SIZE - 1) ? 0 : (ei - ((ei + 1) % VECTOR_SIZE));
-    for (; si < vec_ei; si += VECTOR_SIZE) {
-        values_avx = _mm512_load_pd(&values[si]);
-        res_avx = _mm512_max_pd(res_avx, values_avx);
+    if (ei - si + 1 >= VECTOR_SIZE * 2) {
+        double vec[VECTOR_SIZE] __attribute__((aligned(CACHE_LINE_SIZE)));
+        __m512d values_avx = _mm512_load_pd(&values[si]);
+        _mm512_store_pd(vec, values_avx);
+        __m512d res_avx = _mm512_load_pd(vec);
+
+        size_t vec_ei = (ei - ((ei - si) % VECTOR_SIZE));
+        si += VECTOR_SIZE;
+        for (; si < vec_ei; si += VECTOR_SIZE) {
+            values_avx = _mm512_load_pd(&values[si]);
+            res_avx = _mm512_max_pd(res_avx, values_avx);
+        }
+
+        // find max in the vector
+        _mm512_store_pd(vec, res_avx);
+        for (int i = 0; i < VECTOR_SIZE; ++i) {
+            _AssignIfGreater(res, &vec[i]);
+        }
     }
 
-    // find max in the vector
-    _mm512_store_pd(vec, res_avx);
-    for (int i = 0; i < VECTOR_SIZE; ++i) {
-        _AssignIfGreater(res, &vec[i]);
-    }
-
-    // search in end
+    // search in the remainder
     for (; si <= ei; ++si) {
         _AssignIfGreater(res, &values[si]);
     }
