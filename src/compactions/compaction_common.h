97ccba9bf8145241bafc8d8c84dbe573381e0689
Ofir Moskovich <ofir.moskovich@redislabs.com>
2022-03-23 13:42:06 +0200

    iterators perf improvement including SOA

--- src/compactions/compaction_common.h
+++ src/compactions/compaction_common.h
@@ -11,12 +11,31 @@
 #include <float.h>
 #include <stdint.h>
 
+#define CACHE_LINE_SIZE 64
+#define VECTOR_SIZE (CACHE_LINE_SIZE/sizeof(double))
+
 #define _DOUBLE_MIN (((double)-1.0) * DBL_MAX)
+
+typedef struct MaxMinContext
+{
+    double minValue;
+    double maxValue;
+} MaxMinContext;
 
 static inline void _AssignIfGreater(double *__restrict__ value, double *__restrict__ newValues) {
     if(*newValues > *value) {
         *value = *newValues;
     }
+}
+
+void MaxAppendValuesVec(void *__restrict__ context,
+                        double *__restrict__ values,
+                        size_t si,
+                        size_t ei);
+
+static inline bool is_aligned(void *p, int N)
+{
+    return (uintptr_t)p % N == 0;
 }
 
 #endif
