291f2ef8f185a2cc3096f1eaf26eb047f9d906dd
Ofir Moskovich <ofir.moskovich@redislabs.com>
2022-04-04 17:03:34 +0300

    really inline the max avx impl

--- src/compactions/compaction_common.h
+++ src/compactions/compaction_common.h
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <float.h>
 #include <stdint.h>
+#include "../consts.h"
 #include "compaction_avx512f.h"
 
 #define CACHE_LINE_SIZE 64
@@ -23,7 +24,8 @@ typedef struct MaxMinContext
     double maxValue;
 } MaxMinContext;
 
-static inline void _AssignIfGreater(double *__restrict__ value, double *__restrict__ newValues) {
+static really_inline void _AssignIfGreater(double *__restrict__ value, double *__restrict__ newValues)
+{
     if(*newValues > *value) {
         *value = *newValues;
     }
@@ -34,7 +36,7 @@ void MaxAppendValuesVec(void *__restrict__ context,
                         size_t si,
                         size_t ei);
 
-static inline bool is_aligned(void *p, int N)
+static really_inline bool is_aligned(void *p, int N)
 {
     return (uintptr_t)p % N == 0;
 }
