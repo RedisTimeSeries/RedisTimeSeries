1df3111d0d8bcde1daa036d82e93de148ca8fda7
Ofir Moskovich <ofir.moskovich@redislabs.com>
2022-05-30 18:40:56 +0300

    multi series iterator mem leak fix and assertion removal

--- src/multiseries_agg_dup_sample_iterator.c
+++ src/multiseries_agg_dup_sample_iterator.c
@@ -73,7 +73,6 @@ MultiSeriesAggDupSampleIterator *MultiSeriesAggDupSampleIterator_New(
     newIter->has_next_sample = true;
     if (res != CR_OK) {
         assert(res != CR_ERR); // we don't handle errors in this function currently
-        assert(!isnan(newIter->next_sample.value));
         newIter->has_next_sample = false;
     }
     return newIter;
