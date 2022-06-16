0e0f17774c4f3e7353a720b810555d94133fcfee
Ofir Moskovich <ofir.moskovich@redislabs.com>
2022-06-16 12:15:28 +0300

    add restore of rdb with version smaller than relpicaof
    (cherry picked from commit ec53be9a2f65f00e738713f51a29fdaf82224b90)

--- tests/flow/test_lazy_del.py
+++ tests/flow/test_lazy_del.py
@@ -53,3 +53,42 @@ def test_dump_restore_dst_rule():
         assert len(_get_ts_info(r, key1).rules) == 0
         assert _get_ts_info(r, key2).sourceKey == None
         assert len(_get_ts_info(r, key2).rules) == 0
+
+        r.execute_command('TS.CREATERULE', key1, key2, 'AGGREGATION', 'avg', 60000)
+        assert _get_ts_info(r, key2).sourceKey.decode() == key1
+        assert len(_get_ts_info(r, key1).rules) == 1
+
+        data = r.execute_command('DUMP', key1)
+        r.execute_command('DEL', key1)
+        r.execute_command('restore', key1, 0, data)
+
+        assert _get_ts_info(r, key1).sourceKey == None
+        assert len(_get_ts_info(r, key1).rules) == 0
+        assert _get_ts_info(r, key2).sourceKey == None
+        assert len(_get_ts_info(r, key2).rules) == 0
+
+def test_dump_restore_dst_rule_old_version():
+    with Env().getClusterConnectionIfNeeded() as r:
+        key1 = 'ts1{a}'
+        key2 = 'ts2{a}'
+        r.execute_command('TS.CREATE', key1)
+
+        # data was taken from test_dump_restore_dst_rule on version TS_ALIGNMENT_TS_VER
+        data = b'\x07\x81M \xc1\xf96\x0f\x10\x05\x05\x06ts2{a}\x02\x00\x02P\x00\x02\x02\x02\x00\x04\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x02\x00\x02\x01\x05\x06ts1{a}\x02\x00\x02\x00\x02\x01\x02P\x00\x02\x00\x02\x00\x02\x00\x02\x00\x02\x00\x02\x00\x02\x00\x02 \x02 \x05\xc36P\x00\x01\x00\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0{\x00\x01\x00\x00\x00\t\x00\x1e\x80\x04\xe3\xf0.\xfc$'
+        r.execute_command('restore', key2, 0, data)
+
+        assert _get_ts_info(r, key1).sourceKey == None
+        assert len(_get_ts_info(r, key1).rules) == 0
+        assert _get_ts_info(r, key2).sourceKey == None
+        assert len(_get_ts_info(r, key2).rules) == 0
+
+        r.execute_command('DEL', key1)
+
+        # data was taken from test_dump_restore_dst_rule on version TS_ALIGNMENT_TS_VER
+        data = b'\x07\x81M \xc1\xf96\x0f\x10\x05\x05\x06ts1{a}\x02\x00\x02P\x00\x02\x02\x02\x00\x04\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x02\x00\x02\x00\x02\x00\x02\x01\x05\x06ts2{a}\x02\x80\x00\x00\xea`\x02\x00\x02\x04\x02\x81\xff\xff\xff\xff\xff\xff\xff\xff\x04\x00\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x02\x01\x02P\x00\x02\x00\x02\x00\x02\x00\x02\x00\x02\x00\x02\x00\x02\x00\x02 \x02 \x05\xc36P\x00\x01\x00\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0\xff\x00\xe0{\x00\x01\x00\x00\x00\t\x00\xf9\x7f\x9d\xeeN\xe5\xb3/'
+        r.execute_command('restore', key1, 0, data)
+
+        assert _get_ts_info(r, key1).sourceKey == None
+        assert len(_get_ts_info(r, key1).rules) == 0
+        assert _get_ts_info(r, key2).sourceKey == None
+        assert len(_get_ts_info(r, key2).rules) == 0
