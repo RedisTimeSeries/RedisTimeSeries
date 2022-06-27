5d303381dd998c3d942f1dfca6947554f79dd178
Ofir Moskovich <ofir.moskovich@redislabs.com>
2022-06-27 18:53:02 +0300

    renamed FORCE_SAVE_CROSS_REF to DEUBG_FORCE_RULE_DUMP

--- tests/flow/test_lazy_del.py
+++ tests/flow/test_lazy_del.py
@@ -71,7 +71,7 @@ def test_dump_restore_dst_rule():
 def test_dump_restore_dst_rule_force_save_refs():
     Env().skipOnCluster()
     skip_on_rlec()
-    with Env(moduleArgs='FORCE_SAVE_CROSS_REF enable').getClusterConnectionIfNeeded() as r:
+    with Env(moduleArgs='DEUBG_FORCE_RULE_DUMP enable').getClusterConnectionIfNeeded() as r:
         key1 = 'ts1{a}'
         key2 = 'ts2{a}'
         r.execute_command('TS.CREATE', key1)
