b85681fe81be4ecbf73febb94d99ebd10851fc99
Ofir Moskovich <ofir.moskovich@redislabs.com>
2022-12-19 17:48:12 +0200

    add redis cluster password support

---
+++ tests/flow/test_ts_password.py
@@ -0,0 +1,16 @@
+from RLTest import Env
+from includes import *
+from test_helper_classes import _get_series_value, calc_rule, ALLOWED_ERROR, _insert_data, \
+    _get_ts_info, _insert_agg_data
+
+def test_password():
+    env = Env(moduleArgs='ENCODING UNCOMPRESSED; PASSWORD password')
+    if(not env.isCluster):
+        env.skip()
+    start_ts = 1
+    samples_count = 10
+    with env.getConnection() as r:
+        assert r.execute_command('TS.CREATE', 'tester1{1}', 'LABELS', 'name', 'bob')
+        _insert_data(r, 'tester1{1}', start_ts, samples_count, 1)
+        env.getConnection(1).execute_command('TS.mrange', '-', '+', 'WITHLABELS', 'FILTER',
+                        'name=bob')
