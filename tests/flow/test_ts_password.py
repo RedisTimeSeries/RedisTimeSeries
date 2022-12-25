16226a6344a65b8e709c63328923b3f03374448c
rafie <rafi@redislabs.com>
2022-12-25 19:02:56 +0200

    fixes 1

--- tests/flow/test_ts_password.py
+++ tests/flow/test_ts_password.py
@@ -4,12 +4,13 @@ from test_helper_classes import _get_series_value, calc_rule, ALLOWED_ERROR, _in
     _get_ts_info, _insert_agg_data
 
 def test_password():
-    env = Env(moduleArgs='ENCODING UNCOMPRESSED; PASSWORD password')
+    env = Env(moduleArgs='ENCODING UNCOMPRESSED; OSS_GLOBAL_PASSWORD password')
     if not env.isCluster:
         env.skip()
     start_ts = 1
     samples_count = 10
-    with env.getConnection() as r:
+    with env.getClusterConnectionIfNeeded() as r:
         assert r.execute_command('TS.CREATE', 'tester1{1}', 'LABELS', 'name', 'bob')
         _insert_data(r, 'tester1{1}', start_ts, samples_count, 1)
-        env.getConnection(1).execute_command('TS.mrange', '-', '+', 'WITHLABELS', 'FILTER', 'name=bob')
+        res = env.getConnection(1).execute_command('TS.mrange', '-', '+', 'WITHLABELS', 'FILTER', 'name=bob')
+        print(res)
