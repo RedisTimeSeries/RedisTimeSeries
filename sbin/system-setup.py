3b07b36e06cb0ddd2fe8a28e18468101d1e83d36
rafie <rafi@redislabs.com>
2023-02-16 13:01:33 +0200

    fixes 3

--- sbin/system-setup.py
+++ sbin/system-setup.py
@@ -59,8 +59,8 @@ class RedisTimeSeriesSetup(paella.Setup):
             self.install("lcov-git", aur=True)
         self.run("{READIES}/bin/getaws".format(READIES=READIES))
         if self.pytools:
-            # self.run("{PYTHON} {READIES}/bin/getrmpytools --reinstall --pypi --ramp-version github:redis-py-3.5".format(PYTHON=self.python, READIES=READIES))
-            self.run("{PYTHON} {READIES}/bin/getrmpytools --reinstall --pypi --modern".format(PYTHON=self.python, READIES=READIES))
+            self.run("{PYTHON} {READIES}/bin/getrmpytools --reinstall --pypi --ramp-version github:redis-py-3.5".format(PYTHON=self.python, READIES=READIES))
+            # self.run("{PYTHON} {READIES}/bin/getrmpytools --reinstall --pypi --modern".format(PYTHON=self.python, READIES=READIES))
             self.pip_install("-r {ROOT}/tests/flow/requirements.txt".format(ROOT=ROOT))
             self.run("NO_PY2=1 %s/bin/getpudb" % READIES)
 
