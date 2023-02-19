6920307c054dd42308abe554eb825ceacc15ad1e
Rafi Einstein <rafi@redislabs.com>
2023-02-19 12:14:49 +0200

    [1.8] Build refactoring (#1405)

--- sbin/pack.sh
+++ sbin/pack.sh
@@ -5,6 +5,7 @@ HERE="$(cd "$(dirname "$PROGNAME")" &>/dev/null && pwd)"
 ROOT=$(cd $HERE/.. && pwd)
 export READIES=$ROOT/deps/readies
 . $READIES/shibumi/defs
+
 SBIN=$ROOT/sbin
 
 export PYTHONWARNINGS=ignore
@@ -27,7 +28,7 @@ if [[ $1 == --help || $1 == help || $HELP == 1 ]]; then
 		SYM=0|1           Build debug symbols file
 
 		BRANCH=name       Branch name for snapshot packages
-		VERSION=ver         Version for release packages
+		VERSION=ver       Version for release packages
 		WITH_GITSHA=1     Append Git SHA to shapshot package names
 		VARIANT=name      Build variant (default: empty)
 
@@ -43,19 +44,6 @@ fi
 
 #----------------------------------------------------------------------------------------------
 
-RAMP=${RAMP:-1}
-DEPS=${DEPS:-0}
-SYM=${SYM:-0}
-
-if [[ -z $MODULE || ! -f $MODULE ]]; then
-	eprint "pack: MODULE is not defined or does not refer to a file"
-	exit 1
-fi
-
-[[ -z $ARTDIR ]] && ARTDIR=bin/artifacts
-mkdir -p $ARTDIR $ARTDIR/snapshots
-ARTDIR=$(cd $ARTDIR && pwd)
-
 # RLEC naming conventions
 
 ARCH=$($READIES/bin/platform --arch)
@@ -69,16 +57,34 @@ OSNICK=$($READIES/bin/platform --osnick)
 [[ $OSNICK == xenial ]]  && OSNICK=ubuntu16.04
 [[ $OSNICK == bionic ]]  && OSNICK=ubuntu18.04
 [[ $OSNICK == focal ]]   && OSNICK=ubuntu20.04
-[[ $OSNICK == jammy ]] && OSNICK=ubuntu22.04
+[[ $OSNICK == jammy ]]   && OSNICK=ubuntu22.04
 [[ $OSNICK == centos7 ]] && OSNICK=rhel7
 [[ $OSNICK == centos8 ]] && OSNICK=rhel8
+[[ $OSNICK == ol8 ]]     && OSNICK=rhel8
 [[ $OSNICK == rocky8 ]]  && OSNICK=rhel8
+
+[[ $OSNICK == bigsur ]]  && OSNICK=catalina
+
+#----------------------------------------------------------------------------------------------
+
+if [[ -z $MODULE || ! -f $MODULE ]]; then
+	eprint "pack: MODULE is not defined or does not refer to a file"
+	exit 1
+fi
+
+RAMP=${RAMP:-1}
+DEPS=${DEPS:-0}
+SYM=${SYM:-0}
+
+[[ -z $ARTDIR ]] && ARTDIR=bin/artifacts
+mkdir -p $ARTDIR $ARTDIR/snapshots
+ARTDIR=$(cd $ARTDIR && pwd)
 
 export PRODUCT=redistimeseries
 export PRODUCT_LIB=$PRODUCT.so
 export DEPNAMES=""
 
-export PACKAGE_NAME=redistimeseries
+PACKAGE_NAME=redistimeseries
 
 RAMP_CMD="python3 -m RAMP.ramp"
 
@@ -93,6 +99,8 @@ pack_ramp() {
 	local verspec=${SEMVER}${VARIANT}
 	
 	local fq_package=$stem.${verspec}.zip
+
+	[[ ! -d $ARTDIR ]] && mkdir -p $ARTDIR
 
 	local packfile="$ARTDIR/$fq_package"
 
@@ -135,8 +143,8 @@ pack_deps() {
 	local dep="$1"
 
 	local platform="$OS-$OSNICK-$ARCH"
+	local stem=${PACKAGE_NAME}.${dep}.${platform}
 	local verspec=${SEMVER}${VARIANT}
-	local stem=${PACKAGE_NAME}-${dep}.${platform}
 
 	local depdir=$(cat $ARTDIR/$dep.dir)
 
@@ -160,7 +168,7 @@ pack_deps() {
 	sha256sum $tar_path | awk '{print $1}' > $tar_path.sha256
 
 	cd $ARTDIR/snapshots
-	if [[ ! -z $BRANCH ]]; then
+	if [[ -n $BRANCH ]]; then
 		local snap_dep=$stem.${BRANCH}${VARIANT}.tgz
 		ln -sf ../$fq_dep $snap_dep
 		ln -sf ../$fq_dep.sha256 $snap_dep.sha256
