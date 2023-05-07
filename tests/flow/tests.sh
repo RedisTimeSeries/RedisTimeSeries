7d9d740d34f2249d1737affde0311c76ab876815
rafie <rafi@redislabs.com>
2023-05-07 19:22:57 +0300

    fixes 7

--- tests/flow/tests.sh
+++ tests/flow/tests.sh
@@ -344,11 +344,19 @@ OP=""
 
 #----------------------------------------------------------------------------------------------
 
-GEN=${GEN:-1}
-SLAVES=${SLAVES:-1}
-AOF=${AOF:-1}
-AOF_SLAVES=${AOF_SLAVES:-1}
-OSS_CLUSTER=${OSS_CLUSTER:-0}
+if [[ $QUICK != 1 ]]; then
+	GEN=${GEN:-1}
+	SLAVES=${SLAVES:-1}
+	AOF=${AOF:-1}
+	AOF_SLAVES=${AOF_SLAVES:-1}
+	OSS_CLUSTER=${OSS_CLUSTER:-0}
+else
+	GEN=1
+	SLAVES=0
+	AOF=0
+	AOF_SLAVES=0
+	OSS_CLUSTER=0
+fi
 SHARDS=${SHARDS:-3}
 
 [[ $SAN == addr ]] && SAN=address
@@ -393,13 +401,13 @@ fi
 #----------------------------------------------------------------------------------------------
 
 E=0
-[[ $GEN == 1 ]]    && { (run_tests "general tests"); (( E |= $? )); } || true
-[[ $SLAVES == 1 ]] && { (RLTEST_ARGS="${RLTEST_ARGS} --use-slaves" run_tests "tests with slaves"); (( E |= $? )); } || true
-[[ $AOF == 1 ]]    && { (RLTEST_ARGS="${RLTEST_ARGS} --use-aof" run_tests "tests with AOF"); (( E |= $? )); } || true
-[[ $AOF_SLAVES == 1 ]]    && { (RLTEST_ARGS="${RLTEST_ARGS} --use-aof --use-slaves" run_tests "tests with AOF and slaves"); (( E |= $? )); } || true
+[[ $GEN == 1 ]]         && { (run_tests "general tests"); (( E |= $? )); } || true
+[[ $SLAVES == 1 ]]      && { (RLTEST_ARGS="${RLTEST_ARGS} --use-slaves" run_tests "tests with slaves"); (( E |= $? )); } || true
+[[ $AOF == 1 ]]         && { (RLTEST_ARGS="${RLTEST_ARGS} --use-aof" run_tests "tests with AOF"); (( E |= $? )); } || true
+[[ $AOF_SLAVES == 1 ]]  && { (RLTEST_ARGS="${RLTEST_ARGS} --use-aof --use-slaves" run_tests "tests with AOF and slaves"); (( E |= $? )); } || true
 [[ $OSS_CLUSTER == 1 ]] && { (RLTEST_ARGS="${RLTEST_ARGS} --env oss-cluster --shards-count $SHARDS" run_tests "tests on OSS cluster"); (( E |= $? )); } || true
 
-[[ $RLEC == 1 ]]   && { (RLTEST_ARGS="${RLTEST_ARGS} --env existing-env --existing-env-addr $DOCKER_HOST:$RLEC_PORT" run_tests "tests on RLEC"); (( E |= $? )); } || true
+[[ $RLEC == 1 ]]        && { (RLTEST_ARGS="${RLTEST_ARGS} --env existing-env --existing-env-addr $DOCKER_HOST:$RLEC_PORT" run_tests "tests on RLEC"); (( E |= $? )); } || true
 
 if [[ $NOP != 1 && -n $SAN ]]; then
 	clang_sanitizer_summary
