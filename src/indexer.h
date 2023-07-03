21a29869292d01cdd7f9bb809a10987cc819f71b
Ofir Moskovich <ofir.moskovich@redislabs.com>
2023-07-04 00:21:00 +0300

    improved queryindex performance when there is more than one filter. bug fix: MOD-5338
    (cherry picked from commit 48fba94eddc9a3c192ef7b848003c000388b82ce)

--- src/indexer.h
+++ src/indexer.h
@@ -30,6 +30,8 @@ typedef enum
     // NREQ
 } PredicateType;
 
+#define IS_INCLUSION(type) ((type) == EQ || (type) == CONTAINS || (type) == LIST_MATCH)
+
 typedef struct QueryPredicate
 {
     PredicateType type;
