e7b1b6254750a801698f1d46453133a8f2a2664f
Ofir Moskovich <ofir.moskovich@redislabs.com>
2023-08-17 14:31:39 +0300

    warn fix

--- src/indexer.c
+++ src/indexer.c
@@ -347,7 +347,7 @@ static bool _isKeySatisfyAllPredicates(RedisModuleCtx *ctx,
         }
         free(dicts);
 
-        if (inclusion && !found || !inclusion && found) {
+        if ((inclusion && !found) || (!inclusion && found)) {
             return false;
         }
     }
