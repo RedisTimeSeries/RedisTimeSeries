ba0d06380ee3360ba8974d24143acac5407e0496
alon <alonreshef24@gmail.com>
2023-10-05 12:45:53 +0300

    install deps required for checkout

--- .install/mariner2.sh
+++ .install/mariner2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tdnf build-essential git wget ca-certificates tar openssl-devel cmake python3 python3-pip
+tdnf install -y build-essential git wget ca-certificates tar openssl-devel cmake python3 python3-pip
 
 pip install --upgrade setuptools
 pip install -r tests/flow/requirements.txt
