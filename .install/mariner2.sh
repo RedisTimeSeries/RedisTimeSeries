2ff897f57e77e48c72835144d9abf02db95acee5
alon <alonreshef24@gmail.com>
2023-10-05 14:04:50 +0300

    add which and print curr dir after installing redis

--- .install/mariner2.sh
+++ .install/mariner2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tdnf install -y build-essential wget tar openssl-devel cmake python3 python3-pip
+tdnf install -y build-essential wget tar openssl-devel cmake python3 python3-pip which
 
 pip install --upgrade setuptools
 pip install -r tests/flow/requirements.txt
