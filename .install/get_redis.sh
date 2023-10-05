b0c59849ac679e9997d377004fd39682e61bc939
alon <alonreshef24@gmail.com>
2023-10-05 13:39:48 +0300

    move back to root dir after installing redis

--- .install/get_redis.sh
+++ .install/get_redis.sh
@@ -2,9 +2,14 @@
 
 REDIS_VERSION=7.2.1
 
+curdir="$PWD"
+cd /tmp/
+
 # Download redis source from github archive and extract it.
 wget https://github.com/redis/redis/archive/${REDIS_VERSION}.tar.gz
 tar -xvzf ${REDIS_VERSION}.tar.gz && cd redis-${REDIS_VERSION}
 
 # Build redis from source and install it.
 make && make install
+
+cd $curdir
