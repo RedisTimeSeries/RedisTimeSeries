6ec0bb576bc755503a557c9ab921311d18aabb99
alon <alonreshef24@gmail.com>
2023-10-09 19:20:05 +0300

    CR

--- .install/mariner2.sh
+++ .install/mariner2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tdnf install -y build-essential wget tar openssl-devel cmake python3 python3-pip which unzip
-git config --global --add safe.directory $PWD
+#git config --global --add safe.directory $PWD
 
 pip install --upgrade setuptools
 pip install -r tests/flow/requirements.txt
