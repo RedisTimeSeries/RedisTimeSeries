b86c8303ea22db85b5aacaaddc3e66464a50ef09
YaacovHazan <yaacov.hazan@redis.com>
2023-12-10 11:37:38 +0200

    some change

--- src/version.h
+++ src/version.h
@@ -1,4 +1,5 @@
 /*
+ *Add some change
  *copyright redis ltd. 2017 - present
  *licensed under your choice of the redis source available license 2.0 (rsalv2) or
  *the server side public license v1 (ssplv1).
