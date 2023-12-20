1d98487f5949ba85bec28afc37af84d95b03c5d5
YaacovHazan <yaacov.hazan@redis.com>
2023-12-20 22:06:36 +0200

    fix install script for mariner2
    - add RLTest
    - set ramp-packer version

--- .install/mariner2.sh
+++ .install/mariner2.sh
@@ -4,9 +4,11 @@ tdnf install -y build-essential wget tar openssl-devel cmake python3 python3-pip
 git config --global --add safe.directory $PWD
 
 pip install --upgrade setuptools
+pip install RLTest~=0.4.2
 pip install -r tests/flow/requirements.txt
 
-pip install jinja2 ramp-packer  # required for packing the module (todo: move to pack.sh after refactor)
+pip install git+https://github.com/RedisLabs/RAMP@redis-py-3.5
+pip install jinja2  # required for packing the module (todo: move to pack.sh after refactor)
 
 # Install aws-cli for uploading artifacts to s3
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
