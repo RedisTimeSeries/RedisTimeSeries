aff77deaf239209791f07c2836fd69fda80b078c
Eran Hadad <eranm22@gmail.com>
2024-09-03 19:39:53 +0000

    jammy on GHA

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -3,7 +3,6 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
-pip install -r .install/build_package_requirements.txt  # required for packing the module (todo: move to pack.sh after refactor)
 
 $MODE apt update -qq
 $MODE apt install -yqq git wget build-essential lcov openssl libssl-dev python3 python3-venv python3-dev unzip rsync autoconf automake libtool valgrind
