2c5da09c3d6fede263e5ad68cf8fbd865a9729ea
Meir Shpilraien (Spielrein) <meir@redis.com>
2025-02-09 14:17:44 +0200

    MOD-8596 Working with Redis cluster internal secret (#1687)
    * Working with Redis cluster internal secret
    
    * Fix 'make lint'
    
    * Remove print
    
    * Remove 'ts-global-password' config as it is no longer needed.
    
    * Fix lint
    
    * Remove the modern name from OSS_GLOBAL_PASSWORD and global-password.
    
    * Fix tests
    
    (cherry picked from commit 260f5c3e2b1a705057840b21412249014f9e2c0c)

--- src/libmr_integration.c
+++ src/libmr_integration.c
@@ -558,8 +558,7 @@ static Record *MR_RecordCreate(MRRecordType *type, size_t size) {
 }
 
 int register_rg(RedisModuleCtx *ctx, long long numThreads) {
-    if (MR_Init(ctx, numThreads, TSGlobalConfig.username, TSGlobalConfig.password) !=
-        REDISMODULE_OK) {
+    if (MR_Init(ctx, numThreads, TSGlobalConfig.password) != REDISMODULE_OK) {
         RedisModule_Log(ctx, "warning", "Failed to init LibMR. aborting...");
         return REDISMODULE_ERR;
     }
