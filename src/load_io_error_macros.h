5b5e5e29c8235292392f295c24e16a0ff38dcf9e
Ephraim Feldblum <ephraim.feldblum@redis.com>
2025-05-13 13:46:34 +0300

    __file__ causing problems

--- src/load_io_error_macros.h
+++ src/load_io_error_macros.h
@@ -5,9 +5,9 @@
     auto inline __attribute__((__always_inline__)) void F(int *);                                  \
     int V __attribute__((__cleanup__(F)));                                                         \
     void F(__attribute__((__unused__)) int *_)
-#define DEFERER(F, L, C) DEFER_(DEFER_FUNC_##F##L##C, DEFER_VAR_##F##L##C)
-#define DEFERER_(F, L, C) DEFERER(F, L, C)
-#define defer DEFERER_(__FILE__, __LINE__, __COUNTER__)
+#define DEFERER(L, C) DEFER_(DEFER_FUNC_##L##C, DEFER_VAR_##L##C)
+#define DEFERER_(L, C) DEFERER(L, C)
+#define defer DEFERER_(__LINE__, __COUNTER__)
 #define errdefer(err, ...)                                                                         \
     defer {                                                                                        \
         if (unlikely(err))                                                                         \
