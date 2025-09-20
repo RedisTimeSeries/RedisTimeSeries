9c984fd4c5ca980a1e62bcee60728ad4ae83f072
Tom Gabsow <tom.gabsow@redis.com>
2025-09-20 21:02:07 +0300

    adding TS.ADD

--- src/cmd_info/ts_info.c
+++ src/cmd_info/ts_info.c
@@ -1,6 +1,148 @@
 #include "command_info.h"
 
 // ===============================
+// TS.ADD key timestamp value [options...]
+// ===============================
+static const RedisModuleCommandKeySpec TS_ADD_KEYSPECS[] = {
+    { .notes = "",
+      .flags = REDISMODULE_CMD_KEY_RW | REDISMODULE_CMD_KEY_INSERT,
+      .begin_search_type = REDISMODULE_KSPEC_BS_INDEX,
+      .bs.index = { .pos = 1 },
+      .find_keys_type = REDISMODULE_KSPEC_FK_RANGE,
+      .fk.range = { .lastkey = 0, .keystep = 1, .limit = 0 } },
+    { 0 }
+};
+
+static const RedisModuleCommandArg TS_ADD_ARGS[] = {
+    { .name = "key", .type = REDISMODULE_ARG_TYPE_KEY, .key_spec_index = 0 },
+    { .name = "timestamp", .type = REDISMODULE_ARG_TYPE_STRING },
+    { .name = "value", .type = REDISMODULE_ARG_TYPE_DOUBLE },
+    { .name = "RETENTION",
+      .type = REDISMODULE_ARG_TYPE_BLOCK,
+      .flags = REDISMODULE_CMD_ARG_OPTIONAL,
+      .subargs =
+          (RedisModuleCommandArg[]){
+              { .name = "retentionPeriod", .type = REDISMODULE_ARG_TYPE_INTEGER, .token = "RETENTION" },
+              { 0 } } },
+    { .name = "ENCODING",
+      .type = REDISMODULE_ARG_TYPE_BLOCK,
+      .flags = REDISMODULE_CMD_ARG_OPTIONAL,
+      .subargs =
+          (RedisModuleCommandArg[]){
+              { .name = "ENCODING", .type = REDISMODULE_ARG_TYPE_PURE_TOKEN, .token = "ENCODING" },
+              { .name = "enc",
+                .type = REDISMODULE_ARG_TYPE_ONEOF,
+                .subargs =
+                    (RedisModuleCommandArg[]){ { .name = "COMPRESSED",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "COMPRESSED" },
+                                               { .name = "UNCOMPRESSED",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "UNCOMPRESSED" },
+                                               { 0 } } },
+              { 0 } } },
+    { .name = "CHUNK_SIZE",
+      .type = REDISMODULE_ARG_TYPE_BLOCK,
+      .flags = REDISMODULE_CMD_ARG_OPTIONAL,
+      .subargs =
+          (RedisModuleCommandArg[]){
+              { .name = "size", .type = REDISMODULE_ARG_TYPE_INTEGER, .token = "CHUNK_SIZE" },
+              { 0 } } },
+    { .name = "DUPLICATE_POLICY",
+      .type = REDISMODULE_ARG_TYPE_BLOCK,
+      .flags = REDISMODULE_CMD_ARG_OPTIONAL,
+      .subargs =
+          (RedisModuleCommandArg[]){
+              { .name = "DUPLICATE_POLICY", .type = REDISMODULE_ARG_TYPE_PURE_TOKEN, .token = "DUPLICATE_POLICY" },
+              { .name = "policy",
+                .type = REDISMODULE_ARG_TYPE_ONEOF,
+                .subargs =
+                    (RedisModuleCommandArg[]){ { .name = "BLOCK",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "BLOCK" },
+                                               { .name = "FIRST",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "FIRST" },
+                                               { .name = "LAST",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "LAST" },
+                                               { .name = "MIN",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "MIN" },
+                                               { .name = "MAX",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "MAX" },
+                                               { .name = "SUM",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "SUM" },
+                                               { 0 } } },
+              { 0 } } },
+    { .name = "ON_DUPLICATE",
+      .type = REDISMODULE_ARG_TYPE_BLOCK,
+      .flags = REDISMODULE_CMD_ARG_OPTIONAL,
+      .subargs =
+          (RedisModuleCommandArg[]){
+              { .name = "ON_DUPLICATE", .type = REDISMODULE_ARG_TYPE_PURE_TOKEN, .token = "ON_DUPLICATE" },
+              { .name = "policy_ovr",
+                .type = REDISMODULE_ARG_TYPE_ONEOF,
+                .subargs =
+                    (RedisModuleCommandArg[]){ { .name = "BLOCK",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "BLOCK" },
+                                               { .name = "FIRST",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "FIRST" },
+                                               { .name = "LAST",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "LAST" },
+                                               { .name = "MIN",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "MIN" },
+                                               { .name = "MAX",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "MAX" },
+                                               { .name = "SUM",
+                                                 .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+                                                 .token = "SUM" },
+                                               { 0 } } },
+              { 0 } } },
+    { .name = "IGNORE",
+      .type = REDISMODULE_ARG_TYPE_BLOCK,
+      .flags = REDISMODULE_CMD_ARG_OPTIONAL,
+      .subargs =
+          (RedisModuleCommandArg[]){
+              { .name = "IGNORE", .type = REDISMODULE_ARG_TYPE_PURE_TOKEN, .token = "IGNORE" },
+              { .name = "ignoreMaxTimediff", .type = REDISMODULE_ARG_TYPE_INTEGER },
+              { .name = "ignoreMaxValDiff", .type = REDISMODULE_ARG_TYPE_DOUBLE },
+              { 0 } } },
+    { .name = "LABELS",
+      .type = REDISMODULE_ARG_TYPE_BLOCK,
+      .flags = REDISMODULE_CMD_ARG_OPTIONAL,
+      .subargs =
+          (RedisModuleCommandArg[]){
+              { .name = "LABELS", .type = REDISMODULE_ARG_TYPE_PURE_TOKEN, .token = "LABELS" },
+              { .name = "label_value_pairs", 
+                .type = REDISMODULE_ARG_TYPE_BLOCK,
+                .flags = REDISMODULE_CMD_ARG_MULTIPLE | REDISMODULE_CMD_ARG_OPTIONAL,
+                .subargs = (RedisModuleCommandArg[]){
+                    { .name = "label", .type = REDISMODULE_ARG_TYPE_STRING },
+                    { .name = "value", .type = REDISMODULE_ARG_TYPE_STRING },
+                    { 0 } } },
+              { 0 } } },
+    { 0 }
+};
+
+static const RedisModuleCommandInfo TS_ADD_INFO = {
+    .version = REDISMODULE_COMMAND_INFO_VERSION,
+    .summary = "Append a sample to a time series",
+    .complexity = "O(M) where M is the number of compaction rules or O(1) with no compaction",
+    .since = "1.0.0",
+    .arity = -4,
+    .key_specs = (RedisModuleCommandKeySpec *)TS_ADD_KEYSPECS,
+    .args = (RedisModuleCommandArg *)TS_ADD_ARGS,
+};
+
+// ===============================
 // TS.REVRANGE key fromTimestamp toTimestamp [options...]
 // ===============================
 static const RedisModuleCommandKeySpec TS_REVRANGE_KEYSPECS[] = {
@@ -147,6 +289,16 @@ static const RedisModuleCommandInfo TS_REVRANGE_INFO = {
 };
 
 int RegisterTSCommandInfos(RedisModuleCtx *ctx) {
+    // Register TS.ADD command info
+    RedisModuleCommand *cmd_add = RedisModule_GetCommand(ctx, "TS.ADD");
+    if (!cmd_add) {
+        return REDISMODULE_ERR;
+    }
+    if (RedisModule_SetCommandInfo(cmd_add, &TS_ADD_INFO) == REDISMODULE_ERR) {
+        return REDISMODULE_ERR;
+    }
+
+    // Register TS.REVRANGE command info
     RedisModuleCommand *cmd_revrange = RedisModule_GetCommand(ctx, "TS.REVRANGE");
     if (!cmd_revrange) {
         return REDISMODULE_ERR;
