b84acd499d26b01b95f33e296ced15845a4aa1c3
Gal Cohen <gal@redis.com>
2025-09-25 14:06:47 +0300

    Rearrange cmds in cmd-info alphabetically

--- src/cmd_info/ts_info.c
+++ src/cmd_info/ts_info.c
@@ -1,6 +1,70 @@
 #include "command_info.h"
 
 // ===============================
+// TS.GET key [LATEST]
+// ===============================
+static const RedisModuleCommandKeySpec TS_GET_KEYSPECS[] = {
+    { .flags = REDISMODULE_CMD_KEY_RO,
+      .begin_search_type = REDISMODULE_KSPEC_BS_INDEX,
+      .bs.index = { .pos = 1 },
+      .find_keys_type = REDISMODULE_KSPEC_FK_RANGE,
+      .fk.range = { .lastkey = 0, .keystep = 1, .limit = 0 } },
+    { 0 }
+};
+
+static const RedisModuleCommandArg TS_GET_ARGS[] = {
+    { .name = "key", .type = REDISMODULE_ARG_TYPE_KEY, .key_spec_index = 0 },
+    { .name = "latest",
+      .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
+      .flags = REDISMODULE_CMD_ARG_OPTIONAL,
+      .token = "LATEST" },
+    { 0 }
+};
+
+static const RedisModuleCommandInfo TS_GET_INFO = {
+    .version = REDISMODULE_COMMAND_INFO_VERSION,
+    .summary = "Get the sample with the highest timestamp from a given time series",
+    .complexity = "O(1)",
+    .since = "1.0.0",
+    .arity = -2,
+    .key_specs = (RedisModuleCommandKeySpec *)TS_GET_KEYSPECS,
+    .args = (RedisModuleCommandArg *)TS_GET_ARGS,
+};
+
+// ===============================
+// TS.DELETERULE source_key dest_key
+// ===============================
+static const RedisModuleCommandKeySpec TS_DELETERULE_KEYSPECS[] = {
+    { .flags = REDISMODULE_CMD_KEY_RO,
+      .begin_search_type = REDISMODULE_KSPEC_BS_INDEX,
+      .bs.index = { .pos = 1 },
+      .find_keys_type = REDISMODULE_KSPEC_FK_RANGE,
+      .fk.range = { .lastkey = 0, .keystep = 1, .limit = 0 } },
+    { .flags = REDISMODULE_CMD_KEY_RW,
+      .begin_search_type = REDISMODULE_KSPEC_BS_INDEX,
+      .bs.index = { .pos = 2 },
+      .find_keys_type = REDISMODULE_KSPEC_FK_RANGE,
+      .fk.range = { .lastkey = 0, .keystep = 1, .limit = 0 } },
+    { 0 }
+};
+
+static const RedisModuleCommandArg TS_DELETERULE_ARGS[] = {
+    { .name = "sourceKey", .type = REDISMODULE_ARG_TYPE_KEY, .key_spec_index = 0 },
+    { .name = "destKey", .type = REDISMODULE_ARG_TYPE_KEY, .key_spec_index = 1 },
+    { 0 }
+};
+
+static const RedisModuleCommandInfo TS_DELETERULE_INFO = {
+    .version = REDISMODULE_COMMAND_INFO_VERSION,
+    .summary = "Delete a compaction rule",
+    .complexity = "O(1)",
+    .since = "1.0.0",
+    .arity = 3,
+    .key_specs = (RedisModuleCommandKeySpec *)TS_DELETERULE_KEYSPECS,
+    .args = (RedisModuleCommandArg *)TS_DELETERULE_ARGS,
+};
+
+// ===============================
 // TS.REVRANGE key fromTimestamp toTimestamp
 //  [LATEST]
 //  [FILTER_BY_TS ts...]
@@ -156,76 +220,7 @@ static const RedisModuleCommandInfo TS_REVRANGE_INFO = {
     .args = (RedisModuleCommandArg *)TS_REVRANGE_ARGS,
 };
 
-// ===============================
-// TS.GET key [LATEST]
-// ===============================
-static const RedisModuleCommandKeySpec TS_GET_KEYSPECS[] = {
-    { .flags = REDISMODULE_CMD_KEY_RO,
-      .begin_search_type = REDISMODULE_KSPEC_BS_INDEX,
-      .bs.index = { .pos = 1 },
-      .find_keys_type = REDISMODULE_KSPEC_FK_RANGE,
-      .fk.range = { .lastkey = 0, .keystep = 1, .limit = 0 } },
-    { 0 }
-};
-
-static const RedisModuleCommandArg TS_GET_ARGS[] = {
-    { .name = "key", .type = REDISMODULE_ARG_TYPE_KEY, .key_spec_index = 0 },
-    { .name = "latest",
-      .type = REDISMODULE_ARG_TYPE_PURE_TOKEN,
-      .flags = REDISMODULE_CMD_ARG_OPTIONAL,
-      .token = "LATEST" },
-    { 0 }
-};
-
-static const RedisModuleCommandInfo TS_GET_INFO = {
-    .version = REDISMODULE_COMMAND_INFO_VERSION,
-    .summary = "Get the sample with the highest timestamp from a given time series",
-    .complexity = "O(1)",
-    .since = "1.0.0",
-    .arity = -2,
-    .key_specs = (RedisModuleCommandKeySpec *)TS_GET_KEYSPECS,
-    .args = (RedisModuleCommandArg *)TS_GET_ARGS,
-};
-
-// ===============================
-// TS.DELETERULE source_key dest_key
-// ===============================
-static const RedisModuleCommandKeySpec TS_DELETERULE_KEYSPECS[] = {
-    { .flags = REDISMODULE_CMD_KEY_RO,
-      .begin_search_type = REDISMODULE_KSPEC_BS_INDEX,
-      .bs.index = { .pos = 1 },
-      .find_keys_type = REDISMODULE_KSPEC_FK_RANGE,
-      .fk.range = { .lastkey = 0, .keystep = 1, .limit = 0 } },
-    { .flags = REDISMODULE_CMD_KEY_RW,
-      .begin_search_type = REDISMODULE_KSPEC_BS_INDEX,
-      .bs.index = { .pos = 2 },
-      .find_keys_type = REDISMODULE_KSPEC_FK_RANGE,
-      .fk.range = { .lastkey = 0, .keystep = 1, .limit = 0 } },
-    { 0 }
-};
-
-static const RedisModuleCommandArg TS_DELETERULE_ARGS[] = {
-    { .name = "sourceKey", .type = REDISMODULE_ARG_TYPE_KEY, .key_spec_index = 0 },
-    { .name = "destKey", .type = REDISMODULE_ARG_TYPE_KEY, .key_spec_index = 1 },
-    { 0 }
-};
-
-static const RedisModuleCommandInfo TS_DELETERULE_INFO = {
-    .version = REDISMODULE_COMMAND_INFO_VERSION,
-    .summary = "Delete a compaction rule",
-    .complexity = "O(1)",
-    .since = "1.0.0",
-    .arity = 3,
-    .key_specs = (RedisModuleCommandKeySpec *)TS_DELETERULE_KEYSPECS,
-    .args = (RedisModuleCommandArg *)TS_DELETERULE_ARGS,
-};
-
 int RegisterTSCommandInfos(RedisModuleCtx *ctx) {
-    RedisModuleCommand *cmd_revrange = RedisModule_GetCommand(ctx, "TS.REVRANGE");
-    if (!cmd_revrange ||
-        RedisModule_SetCommandInfo(cmd_revrange, &TS_REVRANGE_INFO) == REDISMODULE_ERR)
-        return REDISMODULE_ERR;
-
     RedisModuleCommand *cmd_get = RedisModule_GetCommand(ctx, "TS.GET");
     if (!cmd_get || RedisModule_SetCommandInfo(cmd_get, &TS_GET_INFO) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
@@ -233,6 +228,11 @@ int RegisterTSCommandInfos(RedisModuleCtx *ctx) {
     RedisModuleCommand *cmd_deleterule = RedisModule_GetCommand(ctx, "TS.DELETERULE");
     if (!cmd_deleterule ||
         RedisModule_SetCommandInfo(cmd_deleterule, &TS_DELETERULE_INFO) == REDISMODULE_ERR)
+        return REDISMODULE_ERR;
+
+    RedisModuleCommand *cmd_revrange = RedisModule_GetCommand(ctx, "TS.REVRANGE");
+    if (!cmd_revrange ||
+        RedisModule_SetCommandInfo(cmd_revrange, &TS_REVRANGE_INFO) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
 
     return REDISMODULE_OK;
