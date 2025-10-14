d2bf0f59dc8e20c8fcf1be19c9bba7c1a4d7e877
Gal Cohen <gal@redis.com>
2025-10-14 13:47:32 +0300

    Fixed do-i-own-key logic re/ Ozan's CR

--- src/indexer.c
+++ src/indexer.c
@@ -413,6 +413,20 @@ static bool _isKeySatisfyAllPredicates(RedisModuleCtx *ctx,
     return true;
 }
 
+static inline bool OwnKeyDuringSharding(RedisModuleString *key) {  // RE version; during non-ASM reshards
+    int slot = RedisModule_ShardingGetKeySlot(key);
+    if (slot < 0)  // sharding config not set
+        return false;
+    int firstSlot, lastSlot;
+    RedisModule_ShardingGetSlotRange(&firstSlot, &lastSlot);
+    return firstSlot <= slot && slot <= lastSlot;
+}
+
+static inline bool OwnKeyDuringASM(RedisModuleString *key) {  // ASM version
+    unsigned int slot = RedisModule_ClusterKeySlot(key);
+    return RedisModule_ClusterCanAccessKeysInSlot(slot);
+}
+
 RedisModuleDict *QueryIndex(RedisModuleCtx *ctx,
                             QueryPredicate *index_predicate,
                             size_t predicate_count,
@@ -462,15 +476,7 @@ RedisModuleDict *QueryIndex(RedisModuleCtx *ctx,
         RedisModuleDictIter *iter = RedisModule_DictIteratorStartC(res, "^", NULL, 0);
         RedisModuleString *currentKey;
         while ((currentKey = RedisModule_DictNext(NULL, iter, NULL)) != NULL) {
-            int slot = RedisModule_ShardingGetKeySlot(currentKey);
-            bool ownCurrentKey = true;
-            if (RedisModule_ClusterCanAccessKeysInSlot != NULL)
-                ownCurrentKey = RedisModule_ClusterCanAccessKeysInSlot(slot);
-            else {  // assume a single slot-range and use the old APIs
-                int firstSlot, lastSlot;
-                RedisModule_ShardingGetSlotRange(&firstSlot, &lastSlot);
-                ownCurrentKey = (firstSlot <= slot && slot <= lastSlot);
-            }
+            bool ownCurrentKey = (isReshardTrimming ? OwnKeyDuringSharding : OwnKeyDuringASM)(currentKey);
             if (!ownCurrentKey) {
                 RedisModule_DictDel(res, currentKey, NULL);
                 RedisModule_DictIteratorReseek(iter, ">", currentKey);
