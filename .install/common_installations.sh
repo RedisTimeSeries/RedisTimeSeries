85893ee9bf9e7a38e90f7c7c2816eed650fefcf7
avivdavid23 <avivdavid2312@gmail.com>
2025-12-29 18:03:14 +0200

    Revert "pip fix"
    This reverts commit bacdeb13d1e8f6837337fe003bf884c3915ff957.

--- .install/common_installations.sh
+++ .install/common_installations.sh
@@ -2,23 +2,15 @@
 set -e
 OS_TYPE=$(uname -s)
 MODE=$1 # whether to install using sudo or not
-if command -v pip3 &> /dev/null; then
-    PIP_CMD="pip3"
-elif command -v pip &> /dev/null; then
-    PIP_CMD="pip"
-else
-    echo "Neither pip3 nor pip is installed."
-    exit 1
-fi
 
-$PIP_CMD install --upgrade pip
-$PIP_CMD install -q --upgrade setuptools
-echo "pip version: $($PIP_CMD --version)"
-echo "pip path: $(which $PIP_CMD)"
+pip3 install --upgrade pip
+pip3 install -q --upgrade setuptools
+echo "pip version: $(pip3 --version)"
+echo "pip path: $(which pip3)"
 
-$PIP_CMD install -q -r tests/flow/requirements.txt
+pip3 install -q -r tests/flow/requirements.txt
 # These packages are needed to build the package
-$PIP_CMD install -q -r .install/build_package_requirements.txt
+pip3 install -q -r .install/build_package_requirements.txt
 
 # List installed packages
-$PIP_CMD list
+pip3 list
