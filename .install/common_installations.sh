bacdeb13d1e8f6837337fe003bf884c3915ff957
avivdavid23 <avivdavid2312@gmail.com>
2025-12-29 17:37:53 +0200

    pip fix

--- .install/common_installations.sh
+++ .install/common_installations.sh
@@ -2,15 +2,23 @@
 set -e
 OS_TYPE=$(uname -s)
 MODE=$1 # whether to install using sudo or not
+if command -v pip3 &> /dev/null; then
+    PIP_CMD="pip3"
+elif command -v pip &> /dev/null; then
+    PIP_CMD="pip"
+else
+    echo "Neither pip3 nor pip is installed."
+    exit 1
+fi
 
-pip3 install --upgrade pip
-pip3 install -q --upgrade setuptools
-echo "pip version: $(pip3 --version)"
-echo "pip path: $(which pip3)"
+$PIP_CMD install --upgrade pip
+$PIP_CMD install -q --upgrade setuptools
+echo "pip version: $($PIP_CMD --version)"
+echo "pip path: $(which $PIP_CMD)"
 
-pip3 install -q -r tests/flow/requirements.txt
+$PIP_CMD install -q -r tests/flow/requirements.txt
 # These packages are needed to build the package
-pip3 install -q -r .install/build_package_requirements.txt
+$PIP_CMD install -q -r .install/build_package_requirements.txt
 
 # List installed packages
-pip3 list
+$PIP_CMD list
