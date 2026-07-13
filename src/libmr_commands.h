afd88f10e59dde96919d8703a41cf19a5e4c0825
Aviv David <40210928+AvivDavid23@users.noreply.github.com>
2026-07-13 15:21:05 +0300

    CP query labels to 8.10 (#2100)
    * MOD-16370 Introduce `TS.QUERYLABELS`: Get a list of labels and label-… (#2090)
    
    * MOD-16370 Intreduce `TS.QUERYLABELS`: Get a list of labels and label-values
    
    * CR + Format
    
    * fix test
    
    * CR
    
    * CR
    
    * format
    
    * CR
    
    * format
    
    * use try calloc
    
    * add GEARS protocol support
    
    * bump to 8.9.90

--- src/libmr_commands.h
+++ src/libmr_commands.h
@@ -22,5 +22,9 @@ typedef struct MData
 int TSDB_mget_MR(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
 int TSDB_queryindex_MR(RedisModuleCtx *ctx, QueryPredicateList *queries);
 int TSDB_mrange_MR(RedisModuleCtx *ctx, RedisModuleString **argv, int argc, bool reverse);
+int TSDB_querylabels_MR(RedisModuleCtx *ctx,
+                        QueryLabelsSubtype subtype,
+                        RedisModuleString *label,
+                        QueryPredicateList *queries);
 
 #endif // REDIS_TIMESERIES_CLEAN_MR_COMMANDS_H
